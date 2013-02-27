a589fa3c6d20e0cd13a0704b02578072f2f6e3ad
adjidedjo <adji.el.djo@gmail.com>
2013-02-27 11:08:24 +0700

    revising comparison by year

--- app/controllers/laporan_cabang_controller.rb
+++ app/controllers/laporan_cabang_controller.rb
@@ -11,7 +11,14 @@ class LaporanCabangController < ApplicationController
   def comparison_by_year
     @cabang_get_id = Cabang.get_id
     @periode = params[:periode]
-    @get_data_classic = LaporanCabang.query_by_date(params[:periode].to_date, 1.year.ago(params[:periode].to_date)).query_classic
+    @get_data_classic = LaporanCabang.query_by_date(params[:periode].to_date, 
+      1.year.ago(params[:periode].to_date))
+    .query_classic unless params[:periode].nil?
+    #   find a week for report weekly
+    @this_week_on_current_year = (params[:periode].to_date - 6.days).to_date
+    @this_week_on_last_year = 1.year.ago(params[:periode].to_date - 6.days).to_date
+    @last_week_on_current_year = 1.weeks.ago(params[:periode].to_date - 6.days).to_date
+    @last_week_on_last_year = 1.year.ago(1.weeks.ago(params[:periode].to_date - 6.days)).to_date
     
     respond_to do |format|
       format.html # index.html.erb
--- app/models/laporan_cabang.rb
+++ app/models/laporan_cabang.rb
@@ -8,8 +8,46 @@ class LaporanCabang < ActiveRecord::Base
   scope :query_by_branch, lambda {|id_cabang| where(:cabang_id => id_cabang)}
   scope :check_invoices, lambda {|date| where(:tanggalsj => date)}
   scope :query_classic, :conditions => ['jenisbrgdisc = ?', "Classic"]
+  scope :query_by_single_date, lambda {|date| where(:tanggalsj => date)}
   
   def self.query_by_year_and_cabang_id(year, cabang_id)
     sum(:jumlah, :conditions => ["tanggalfaktur >= ? and tanggalfaktur <= ? and cabang_id = ?", "#{year}-01-01", "#{year}-12-31", cabang_id])
   end
+  
+  def self.summaries(date)
+    sum(:harganetto2, :conditions => {:tanggalsj => date})
+  end
+  
+  def self.monthly_sum_last_year(date)
+    query_by_date(get_last_month_on_last_year(date), date).sum(:harganetto2).to_i
+  end
+  
+  def self.monthly_sum_current_year(date)
+    query_by_date(get_last_month_on_current_year(date), date).sum(:harganetto2).to_i
+  end
+  
+  def self.yearly_sum_last_year(date)
+    query_by_date(get_beginning_of_year_on_last_year(date), date).sum(:harganetto2).to_i
+  end
+  
+  def self.yearly_sum_current_year(date)
+    query_by_date(get_beginning_of_year_on_current_year(date), date).sum(:harganetto2).to_i
+  end
+  
+  def self.get_last_month_on_last_year(date)
+    1.year.ago(date.to_date.beginning_of_month).to_date
+  end
+  
+  def self.get_last_month_on_current_year(date)
+    date.to_date.beginning_of_month.to_date
+  end
+  
+  def self.get_beginning_of_year_on_last_year(date)
+    1.year.ago(date.to_date.beginning_of_year).to_date
+  end
+  
+  def self.get_beginning_of_year_on_current_year(date)
+    date.to_date.beginning_of_year.to_date
+  end
+ 
 end
