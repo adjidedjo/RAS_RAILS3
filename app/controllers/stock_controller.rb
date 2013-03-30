720845deaf4f8af08b96294dea20b30d42ebf6ce
IT-MARKETING <it-marketing@ubuntu.(none)>
2013-03-30 10:04:08 +0700

    adding autorization

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,7 +1,7 @@
 class StockController < ApplicationController
   def index
     @id_cabang = Cabang.get_id
-    @get_stock = Stock.get_record(params[:date]) unless params[:date].nil?
+    @get_stock = Stock.get_record(params[:date], current_user) unless params[:date].nil?
   end
   
   def update_kategori
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,18 +2,21 @@ class Stock < ActiveRecord::Base
   set_table_name "tbstockcabang"
   belongs_to :cabang
   belongs_to :barang
-  scope :check_stock, lambda {|date| where(:tanggal => date)}
-  scope :find_kodebrg, lambda {|kodebrg| where(:kodebrg => kodebrg)}
   scope :find_cabang_id, lambda {|cabang_id| where(:cabang_id => cabang_id)}
-  
-  validates :date, :presence => true
 
-	def self.get_record(date)
-		find(:all, :select => "cabang_id, kodebrg, namabrg, freestock, bufferstock",:conditions => ["tanggal = ?", date], :group => :kodebrg, :order => "id DESC")
+	def self.get_record(date, current_user)
+    if current_user.merk.nil?
+      find(:all, :select => "cabang_id, kodebrg, namabrg, freestock, bufferstock",
+        :conditions => ["tanggal = ?", date], :order => "id DESC")
+    else
+      find(:all, :select => "cabang_id, kodebrg, namabrg, freestock, bufferstock",
+        :conditions => ["tanggal = ? and kodebrg like ?", date, "%#{current_user.merk.merk_id}%"],
+        :order => "id DESC")
+    end
 	end
   
   def self.get_stock_in_branch(date, stock)
-    check_stock(date).find_kodebrg(stock)
+    find(:all, :select => "tanggal, cabang_id, kodebrg, freestock, bufferstock",
+      :conditions => ["tanggal = ? and kodebrg like ?", date, stock])
   end
 end
-
