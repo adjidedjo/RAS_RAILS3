172d95b5098ea93f99f9fe9dcdfa5f0f9bb14deb
IT-MARKETING <it-marketing@ubuntu.(none)>
2013-05-14 09:56:18 +0700

    adding gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,3 +16,18 @@ set :user, "marketing"
 set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
