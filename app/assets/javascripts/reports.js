511343c41d5ada32c620bf5b9d0a462298586217
adjidedjo <itmarketing@itmarketing-K45A.(none)>
2013-09-16 14:15:01 +0700

    revising table design

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -62,6 +62,159 @@ $(document).ready(function(){
             }
         }
     });
+    
+		
+    $('#cus_compare').dataTable({
+        iDisplayLength: 10,
+        bRetrieve: true
+//        "fnFooterCallback": function ( nRow, aaData ) {
+//            /*
+//             * Calculate the total market share for all browsers in this table (ie inc. outside
+//             * the pagination)
+//             */
+//
+//            var iTotalQtyCity = 0;
+//            
+//            var iTotalQtyJanuary = 0;
+//            var iTotalValueJanuary = 0;
+//
+//            var iTotalQtyFebruary = 0;
+//            var iTotalValueFebruary = 0;
+//
+//            var iTotalQtyMarch = 0;
+//            var iTotalValueMarch = 0;
+//
+//            var iTotalQtyApril = 0;
+//            var iTotalValueApril = 0;
+//
+//            var iTotalQtyMay = 0;
+//            var iTotalValueMay = 0;
+//
+//            var iTotalQtyJune = 0;
+//            var iTotalValueJune = 0;
+//
+//            for ( var i=0 ; i<aaData.length ; i++ )
+//            {
+//                
+//                iTotalQtyJanuary += parseCurrency(aaData[i][2])*1;
+//                iTotalValueJanuary += parseCurrency(aaData[i][3])*1;
+//
+//                iTotalQtyFebruary += parseCurrency(aaData[i][4])*1;
+//                iTotalValueFebruary += parseCurrency(aaData[i][5])*1;
+//
+//                iTotalQtyMarch += parseCurrency(aaData[i][6])*1;
+//                iTotalValueMarch += parseCurrency(aaData[i][7])*1;
+//
+//                iTotalQtyApril += parseCurrency(aaData[i][8])*1;
+//                iTotalValueApril += parseCurrency(aaData[i][9])*1;
+//
+//                iTotalQtyMay += parseCurrency(aaData[i][10])*1;
+//                iTotalValueMay += parseCurrency(aaData[i][11])*1;
+//
+//                iTotalQtyJune += parseCurrency(aaData[i][12])*1;
+//                iTotalValueJune += parseCurrency(aaData[i][13])*1;
+//            }
+
+            /* Modify the footer row to match what we want */
+//            var nCells = nRow.getElementsByTagName('td');
+////            nCells[1].innerHTML = addCommas(parseInt(iTotalQtyCity))
+//            
+////            nCells[0].innerHTML = addCommas(parseInt(iTotalQtyJanuary))
+//            nCells[1].innerHTML = addCommas(parseInt(iTotalValueJanuary))
+//
+//            nCells[2].innerHTML = addCommas(parseInt(iTotalQtyFebruary))
+//            nCells[3].innerHTML = addCommas(parseInt(iTotalValueFebruary))
+//
+//            nCells[4].innerHTML = addCommas(parseInt(iTotalQtyMarch))
+//            nCells[5].innerHTML = addCommas(parseInt(iTotalValueMarch))
+//
+//            nCells[6].innerHTML = addCommas(parseInt(iTotalQtyApril))
+//            nCells[7].innerHTML = addCommas(parseInt(iTotalValueApril))
+//
+//            nCells[8].innerHTML = addCommas(parseInt(iTotalQtyMay))
+//            nCells[9].innerHTML = addCommas(parseInt(iTotalValueMay))
+//
+//            nCells[10].innerHTML = addCommas(parseInt(iTotalQtyJune))
+//            nCells[11].innerHTML = addCommas(parseInt(iTotalValueJune))
+//            nCells[12].innerHTML = addCommas(parseInt(iTotalValueJanuary))
+//        }
+    });
+    $('#cus_compare2').dataTable({
+        
+        iDisplayLength: 10,
+        bRetrieve: true
+//        "fnFooterCallback": function ( nRow, aaData ) {
+//            /*
+//             * Calculate the total market share for all browsers in this table (ie inc. outside
+//             * the pagination)
+//             */
+//            var iTotalQtyCity = 0;
+//            
+//            var iTotalQtyJuly = 0;
+//            var iTotalValueJuly = 0;
+//
+//            var iTotalQtyAugust = 0;
+//            var iTotalValueAugust = 0;
+//
+//            var iTotalQtySeptember = 0;
+//            var iTotalValueSeptember = 0;
+//
+//            var iTotalQtyOctober = 0;
+//            var iTotalValueOctober = 0;
+//
+//            var iTotalQtyNovember = 0;
+//            var iTotalValueNovember = 0;
+//
+//            var iTotalQtyDecember = 0;
+//            var iTotalValueDecember = 0;
+//
+//            for ( var i=0 ; i<aaData.length ; i++ )
+//            {
+////                iTotalQtyCity += parseCurrency(aaData[i][2])*1;
+//                
+//                iTotalQtyJuly += parseCurrency(aaData[i][2])*1;
+//                iTotalValueJuly += parseCurrency(aaData[i][3])*1;
+//
+//                iTotalQtyAugust += parseCurrency(aaData[i][4])*1;
+//                iTotalValueAugust += parseCurrency(aaData[i][5])*1;
+//
+//                iTotalQtySeptember += parseCurrency(aaData[i][6])*1;
+//                iTotalValueSeptember += parseCurrency(aaData[i][7])*1;
+//
+//                iTotalQtyOctober += parseCurrency(aaData[i][8])*1;
+//                iTotalValueOctober += parseCurrency(aaData[i][9])*1;
+//
+//                iTotalQtyNovember += parseCurrency(aaData[i][10])*1;
+//                iTotalValueNovember += parseCurrency(aaData[i][11])*1;
+//
+//                iTotalQtyDecember += parseCurrency(aaData[i][12])*1;
+//                iTotalValueDecember += parseCurrency(aaData[i][13])*1;
+//            }
+
+            /* Modify the footer row to match what we want */
+//            var nCells = nRow.getElementsByTagName('td');
+////            nCells[1].innerHTML = addCommas(parseInt(iTotalQtyCity))
+//            
+//            nCells[2].innerHTML = addCommas(parseInt(iTotalQtyJuly))
+//            nCells[3].innerHTML = addCommas(parseInt(iTotalValueJuly))
+//
+//            nCells[4].innerHTML = addCommas(parseInt(iTotalQtyAugust))
+//            nCells[5].innerHTML = addCommas(parseInt(iTotalValueAugust))
+//
+//            nCells[6].innerHTML = addCommas(parseInt(iTotalQtySeptember))
+//            nCells[7].innerHTML = addCommas(parseInt(iTotalValueSeptember))
+//
+//            nCells[8].innerHTML = addCommas(parseInt(iTotalQtyOctober))
+//            nCells[9].innerHTML = addCommas(parseInt(iTotalValueOctober))
+//
+//            nCells[10].innerHTML = addCommas(parseInt(iTotalQtyNovember))
+//            nCells[11].innerHTML = addCommas(parseInt(iTotalValueNovember))
+//
+//            nCells[12].innerHTML = addCommas(parseInt(iTotalQtyDecember))
+//            nCells[13].innerHTML = addCommas(parseInt(iTotalValueDecember))
+
+//        }
+    });
 		
     // Add a tabletool to export to pdf, excel and csv
     $('#detail_report').dataTable({
