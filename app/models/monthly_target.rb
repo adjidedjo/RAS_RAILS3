c7438ff0fff6d9a3b0a59f557ef550303a0a0a53
adjidedjo <itmarketing@itmarketing-K45A.(none)>
2014-03-08 08:54:14 +0700

    revise weekly report

--- app/models/monthly_target.rb
+++ app/models/monthly_target.rb
@@ -1,13 +1,15 @@
 class MonthlyTarget < ActiveRecord::Base
-	has_many :months
+	 has_many :months
   accepts_nested_attributes_for :months, :allow_destroy => true
-	belongs_to :cabang
+	 belongs_to :cabang
+  belongs_to :saleman
+  belongs_to :merk
 
-	validates_uniqueness_of :target_year,:cabang_id, :brand_id, :scope => [:target_year, :cabang_id, :brand_id], 
+	 validates_uniqueness_of :target_year, :cabang_id, :merk_id, :sales_id, :customer, :scope => [:target_year, :cabang_id, :merk_id, :sales_id, :customer], 
 		:message => "should happen once per year"
 
-	def self.get_monthly_target(brand, cabang, month, year)
-		find(:all, :select => "total", :conditions => ["brand_id = ? and cabang_id = ? and target_year = ?", 
-			brand, cabang, %(#{year +'-01-'+'01'})])
-	end
+	 def self.get_monthly_target(merk, cabang, month, year)
+		 find(:all, :select => "total", :conditions => ["merk_id = ? and cabang_id = ? and target_year = ?", 
+			 merk, cabang, %(#{year +'-01-'+'01'})])
+	 end
 end
