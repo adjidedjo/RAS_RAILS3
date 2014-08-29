90ab60257de978a7b35976a56c3fbb1ee960f8a4
adjidedjo <adji.el.djo@gmail.com>
2014-08-29 09:01:45 +0700

    revise db structur

---
+++ app/models/brand_branch.rb
@@ -0,0 +1,12 @@
+class BrandBranch < ActiveRecord::Base
+  set_table_name "brand"
+  
+  scope :not_equal_with_nosj, where("nosj not like ? and nosj not like ? and nosj not like ? and nosj not like ? and ketppb not like ?", %(#{'SJB'}%), %(#{'SJY'}%), %(#{'SJV'}%), %(#{'SJP'}%), %(#{'RD'}%))
+	scope :search_by_month_and_year, lambda { |month, year| where("bulan = ? and tahun = ?", month, year)}
+	scope :search_by_branch, lambda {|branch| where("cabang_id in (?)", branch) if branch.present? }
+  scope :brand, lambda {|brand| where("brand in (?)", brand) if brand.present?}
+	
+  def self.customer_quick_monthly(month, year, branch, brand)
+    search_by_month_and_year(month, year).search_by_branch(branch).brand(brand)
+	end
+end
