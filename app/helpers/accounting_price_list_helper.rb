cad757992f19bd3cd94533eb5a7f33bda49d404e
adjidedjo <adji.el.djo@gmail.com>
2014-10-02 13:45:17 +0700

    revise check price list

---
+++ app/helpers/accounting_price_list_helper.rb
@@ -0,0 +1,7 @@
+module AccountingPriceListHelper
+  def options_for_tipe_price
+    ["Non Serenity", "Serenity", "Lady Americana", "Royal", "Classic", "Grand",
+      "Clearance Sale"
+    ]
+  end
+end
--- app/models/accounting_price_list.rb
+++ app/models/accounting_price_list.rb
@@ -2,7 +2,7 @@ class AccountingPriceList < ActiveRecord::Base
   set_table_name "checked_item_masters"
   
   scope :search_by_month_and_year, lambda { |month, year| where("MONTH(tgl_faktur) = ? and YEAR(tgl_faktur) = ?", month, year)}
-	scope :not_equal_with_nofaktur, where("nofaktur not like ? and nofaktur not like ? and nofaktur not like ? and nofaktur not like ? and nofaktur not like ?", %(#{'FKD'}%), %(#{'FKB'}%), %(#{'FKY'}%), %(#{'FKV'}%), %(#{'FKP'}%))
+	scope :not_equal_with_nofaktur, where("nofaktur not like ? and nofaktur not like ? and nofaktur not like ? and nofaktur not like ?", %(#{'FKB'}%), %(#{'FKY'}%), %(#{'FKV'}%), %(#{'FKP'}%))
 	scope :no_return, where("nofaktur not like ? and nofaktur not like ? ", %(#{'RTR'}%),%(#{'RET'}%))
 	scope :brand, lambda {|brand| where("nofaktur like ?", %(%#{brand}%)) if brand.present?}
 	scope :search_by_branch, lambda {|branch| where("cabang_id in (?)", branch) if branch.present? }
