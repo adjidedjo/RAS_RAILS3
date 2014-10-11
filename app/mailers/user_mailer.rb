3cfd8cceb10cc21de6ddfbb6da6d00741a253cdc
adjidedjo <adji.el.djo@gmail.com>
2014-10-11 09:14:03 +0700

    revise recipient mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,11 @@ class UserMailer < ActionMailer::Base
 	def report
     email1 = "aji.y@ras.co.id"
     email2 = "ratna.d@ras.co.id"
-    email2 = "irne@ras.co.id"
-    email2 = "frieder@ras.co.id"
-    email2 = "rudy.s@ras.co.id"
-    email2 = "daniel@ras.co.id"
-    recipients = email1, email2
+    email3 = "irne@ras.co.id"
+    email4 = "frieder@ras.co.id"
+    email5 = "rudy.s@ras.co.id"
+    email6 = "daniel@ras.co.id"
+    recipients = email1, email2, email3, email4, email5, email6
 		@dates = Date.today.beginning_of_month..Date.today
     @cabang_get_id_first = Cabang.get_id_to_7
     @cabang_get_id_second = Cabang.get_id_to_22
