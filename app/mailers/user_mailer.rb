4b2f7e53cfd832a34c6eb7dd58f9b58337b6a909
adjidedjo <adji.el.djo@gmail.com>
2014-10-20 10:49:45 +0700

    revise scheduler

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,13 @@ class UserMailer < ActionMailer::Base
     email1 = "aji.y@ras.co.id"
     email2 = "ratna.d@ras.co.id"
     email3 = "irne@ras.co.id"
-    email4 = "friedermorulak@ras.co.id"
+    email4 = "daniel@ras.co.id"
     email5 = "rudy.s@ras.co.id"
-    email6 = "daniel@ras.co.id"
-    recipients = email1, email2, email3, email4, email5, email6
+    recipients = email1, email2, email3, email4, email5
 		@dates = Date.today.beginning_of_month..Date.today
     @cabang_get_id_first = Cabang.get_id_to_7
     @cabang_get_id_second = Cabang.get_id_to_22
-    mail(:to => email1, :subject => "Laporan Upload Data Penjualan Harian")
+    mail(:to => recipients.join(','), :subject => "Laporan Upload Data Penjualan Harian")
   end
   
   def account_approved(user)
