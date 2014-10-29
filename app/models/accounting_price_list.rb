7231a3fb53cbb0170813a95129a44424b0af13d5
adjidedjo <adji.el.djo@gmail.com>
2014-10-29 13:31:27 +0700

    revise by brand check price list

--- app/models/accounting_price_list.rb
+++ app/models/accounting_price_list.rb
@@ -4,6 +4,6 @@ class AccountingPriceList < ActiveRecord::Base
   scope :search_by_month_and_year, lambda { |month, year| where("MONTH(tgl_faktur) = ? and YEAR(tgl_faktur) = ?", month, year)}
 	scope :not_equal_with_nofaktur, where("nofaktur not like ? and nofaktur not like ? and nofaktur not like ? and nofaktur not like ?", %(#{'FKB'}%), %(#{'FKY'}%), %(#{'FKV'}%), %(#{'FKP'}%))
 	scope :no_return, where("nofaktur not like ? and nofaktur not like ? ", %(#{'RTR'}%),%(#{'RET'}%))
-	scope :brand, lambda {|brand| where("nofaktur like ?", %(%#{brand}%)) if brand.present?}
+	scope :brand, lambda {|brand| where("kodebarang like ?", %(__#{brand}%)) if brand.present?}
 	scope :search_by_branch, lambda {|branch| where("cabang_id in (?)", branch) if branch.present? }
 end
