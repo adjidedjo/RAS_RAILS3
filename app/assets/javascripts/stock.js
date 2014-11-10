86b105cf9fde5f688e6cce52c56e505183d65962
adjidedjo <adji.el.djo@gmail.com>
2014-11-10 08:20:01 +0700

    revise validation for stock

--- app/assets/javascripts/stock.js
+++ app/assets/javascripts/stock.js
@@ -3,6 +3,44 @@
  * and open the template in the editor.
  */
 $(document).ready(function(){
+  
+  $('.stock_index input[type="text"]').tooltipster({ 
+    trigger: 'custom', // default is 'hover' which is no good here
+    onlyOne: false,    // allow multiple tips to be open at a time
+    position: 'right'  // display the tips to the right of the element
+  });
+    
+  $('.stock_index').validate({
+    errorPlacement: function (error, element) {
+                        
+      var lastError = $(element).data('lastError'),
+      newError = $(error).text();
+            
+      $(element).data('lastError', newError);
+                            
+      if(newError !== '' && newError !== lastError){
+        $(element).tooltipster('content', newError);
+        $(element).tooltipster('show');
+      }
+    },
+    success: function (label, element) {
+      $(element).tooltipster('hide');
+      $(element).closest('.control-group').removeClass('error').addClass('success');
+    },
+    highlight: function (element) {
+      $(element).closest('.control-group').removeClass('success').addClass('error');
+    },
+    rules: {
+      date: {
+        required: true
+      }
+    },
+    messages:{
+      date:{
+        required: "Pilih Tanggal"
+      }
+    }
+  });
 
   $('#date').datepicker({
     dateFormat: 'yy-mm-dd'
