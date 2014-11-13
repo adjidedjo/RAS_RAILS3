838ceab3b2f2f637a13da04e78d520e5eb599b3b
adjidedjo <adji.el.djo@gmail.com>
2014-11-13 10:29:21 +0700

    sqlserver to mysql

--- app/workers/control_upload.rb
+++ app/workers/control_upload.rb
@@ -1,5 +1,5 @@
 class ControlUpload
-  
+
   class << self
 
     def perform(method)
@@ -28,11 +28,11 @@ class ControlUpload
   def send_mail
     UserMailer.report.deliver
   end
-  
+
   def send_mail_previous_month
     UserMailer.report_previous_month.deliver
   end
-  
+
   def update_price_list
     Regional.update_harga
     Regional.update_discount
@@ -40,22 +40,24 @@ class ControlUpload
     Regional.update_cashback
     Regional.update_special_price
   end
-  
+
   def check_reports
-    PriceList.check_availability_master(Date.today.prev_month.month, Date.today.month, Date.today.prev_month.year, Date.today.year)
+    SqlSales.migration_sales_report(Date.today.month, Date.today.year)
     LaporanCabang.create_new_artikel_from_report(Date.today.month, Date.today.year)
+    PriceList.check_availability_master(Date.today.prev_month.month, Date.today.month, Date.today.prev_month.year, Date.today.year)
     PriceList.check_report_price_list(Date.today.prev_month.month, Date.today.month, Date.today.prev_month.year, Date.today.year)
-    LaporanCabang.sales_by_brand(Date.today.month, Date.today.year)  
-    LaporanCabang.sales_by_product(Date.today.month, Date.today.year)  
-    LaporanCabang.sales_by_article(Date.today.month, Date.today.year)  
-    LaporanCabang.sales_by_fabric(Date.today.month, Date.today.year)  
-    LaporanCabang.sales_by_customer(Date.today.month, Date.today.year)  
-    LaporanCabang.sales_by_salesmen(Date.today.month, Date.today.year)  
-    LaporanCabang.sales_by_size(Date.today.month, Date.today.year)  
+    LaporanCabang.sales_by_brand(Date.today.month, Date.today.year)
+    LaporanCabang.sales_by_product(Date.today.month, Date.today.year)
+    LaporanCabang.sales_by_article(Date.today.month, Date.today.year)
+    LaporanCabang.sales_by_fabric(Date.today.month, Date.today.year)
+    LaporanCabang.sales_by_customer(Date.today.month, Date.today.year)
+    LaporanCabang.sales_by_salesmen(Date.today.month, Date.today.year)
+    LaporanCabang.sales_by_size(Date.today.month, Date.today.year)
   end
-  
+
   def check_reports_prev_month
-    LaporanCabang.sales_by_brand(1.month.ago.beginning_of_month.to_date.month, 1.month.ago.beginning_of_month.to_date.year)  
+    SqlSales.migration_sales_report(1.month.ago.beginning_of_month.to_date.month, 1.month.ago.beginning_of_month.to_date.year)
+    LaporanCabang.sales_by_brand(1.month.ago.beginning_of_month.to_date.month, 1.month.ago.beginning_of_month.to_date.year)
     LaporanCabang.sales_by_product(1.month.ago.beginning_of_month.to_date.month, 1.month.ago.beginning_of_month.to_date.year)
     LaporanCabang.sales_by_article(1.month.ago.beginning_of_month.to_date.month, 1.month.ago.beginning_of_month.to_date.year)
     LaporanCabang.sales_by_fabric(1.month.ago.beginning_of_month.to_date.month, 1.month.ago.beginning_of_month.to_date.year)
