43469a1aae217d9f8c19f8c0413384416a10e731
adjidedjo <adji.el.djo@gmail.com>
2014-11-15 10:36:51 +0700

    revise mailer for sales

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,29 +2,29 @@ class UserMailer < ActionMailer::Base
   default from: "Admin Sales Analytic"
 
 	def report
-    email1 = "aji.y@ras.co.id"
-    email2 = "ratna.d@ras.co.id"
-    email3 = "irne@ras.co.id"
-    email4 = "daniel@ras.co.id"
-    email5 = "rudy.s@ras.co.id"
-    recipients = email1, email2, email3, email4, email5
+    recipient_to = []
+    recipient_cc = []
+    Recipient.where("mailing_for in (?)", ["penjualan", "all"]).each do |rec|
+      recipient_to << rec.email if rec.cc == false
+      recipient_cc << rec.email if rec.cc == true
+    end
 		@dates = Date.today.beginning_of_month..Date.today
     @cabang_get_id_first = Cabang.get_id_to_7
     @cabang_get_id_second = Cabang.get_id_to_22
-    mail(:to => recipients, :subject => "Laporan Upload Data Penjualan Harian")
+    mail(:to => recipient_to, :cc => recipient_cc,:subject => "Laporan Upload Data Penjualan Harian")
   end
 
   def report_previous_month
-    email1 = "aji.y@ras.co.id"
-    email2 = "ratna.d@ras.co.id"
-    email3 = "irne@ras.co.id"
-    email4 = "daniel@ras.co.id"
-    email5 = "rudy.s@ras.co.id"
-    recipients = email1, email2, email3, email4, email5
+    recipient_to = []
+    recipient_cc = []
+    Recipient.where("mailing_for in (?)", ["penjualan", "all"]).each do |rec|
+      recipient_to << rec.email if rec.cc == false
+      recipient_cc << rec.email if rec.cc == true
+    end
 		@dates = 1.month.ago.beginning_of_month.to_date..1.month.ago.end_of_month.to_date
     @cabang_get_id_first = Cabang.get_id_to_7
     @cabang_get_id_second = Cabang.get_id_to_22
-    mail(:to => recipients, :subject => "Laporan Upload Data Penjualan Harian Bulan Lalu")
+    mail(:to => recipient_to, :cc => recipient_cc, :subject => "Laporan Upload Data Penjualan Harian Bulan Lalu")
   end
 
   def account_approved(user)
