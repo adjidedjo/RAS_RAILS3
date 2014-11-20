867187554ef0ec684013c4a811e1572ffa689198
adjidedjo <adji.el.djo@gmail.com>
2014-11-20 15:57:13 +0700

    revise view price list

--- app/models/accounting_price_list.rb
+++ app/models/accounting_price_list.rb
@@ -1,7 +1,7 @@
 class AccountingPriceList < ActiveRecord::Base
   set_table_name "checked_item_masters"
-  
-  scope :search_by_month_and_year, lambda { |month, year| where("MONTH(tgl_faktur) = ? and YEAR(tgl_faktur) = ?", month, year)}
+
+  scope :search_by_month_and_year, lambda { |month, year| where("MONTH(tanggal) = ? and YEAR(tanggal) = ?", month, year)}
 	scope :not_equal_with_nofaktur, where("nofaktur not like ? and nofaktur not like ? and nofaktur not like ? and nofaktur not like ?", %(#{'FKB'}%), %(#{'FKY'}%), %(#{'FKV'}%), %(#{'FKP'}%))
 	scope :no_return, where("nofaktur not like ? and nofaktur not like ? ", %(#{'RTR'}%),%(#{'RET'}%))
 	scope :brand, lambda {|brand| where("kodebarang like ?", %(__#{brand}%)) if brand.present?}
