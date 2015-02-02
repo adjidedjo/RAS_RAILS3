1be7ad1e16f1d0652c326791fd9a95d12ff57b21
adjidedjo <adji.el.djo@gmail.com>
2015-02-02 10:48:43 +0700

    add input target

--- app/models/monthly_target.rb
+++ app/models/monthly_target.rb
@@ -3,7 +3,7 @@ class MonthlyTarget < ActiveRecord::Base
   belongs_to :salesman
   belongs_to :merk
 
-  validates_uniqueness_of :target_year, :cabang_id, :merk_id, :customer, :scope => [:target_year, :cabang_id, :merk_id, :sales, :customer], 
+  validates_uniqueness_of :target_year, :cabang_id, :merk_id, :customer, :scope => [:target_year, :cabang_id, :merk_id, :sales, :customer],
 		:message => "should happen once per year"
 
   scope :branch, lambda {|branch| where(:cabang_id => branch) if branch.present?}
@@ -15,17 +15,17 @@ class MonthlyTarget < ActiveRecord::Base
   scope :date, lambda {|date| where(:target_year => date.year) if date.present?}
 
   def self.get_monthly_target(merk, cabang, month, year)
-    find(:all, :select => "total", :conditions => ["merk_id = ? and cabang_id = ? and target_year = ?", 
+    find(:all, :select => "total", :conditions => ["merk_id = ? and cabang_id = ? and target_year = ?",
         merk, cabang, %(#{year +'-01-'+'01'})])
   end
 
   def self.get_target_by_branch(cabang, merk, date)
-    month_selected = date.to_date.strftime("%B").downcase 
-    select("cabang_id, #{month_selected}").branch(cabang).merk(merk).group("cabang_id").sum("#{month_selected}") 
+    month_selected = date.to_date.strftime("%B").downcase
+    select("cabang_id, #{month_selected}").branch(cabang).merk(merk).date(date.to_date).group("cabang_id").sum("#{month_selected}")
   end
 
   def self.get_target_by_sales(cabang, merk, date)
-    month_selected = date.to_date.strftime("%B").downcase 
-    select("cabang_id, sales, #{month_selected}, merk_id").branch(cabang).merk(merk)
+    month_selected = date.to_date.strftime("%B").downcase
+    select("cabang_id, sales, #{month_selected}, merk_id").branch(cabang).merk(merk).date(date.to_date)
   end
 end
