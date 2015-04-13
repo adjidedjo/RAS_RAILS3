9865fb72f7c4caba39c87fa8a26df8fbd9331d29
adjidedjo <adji.el.djo@gmail.com>
2015-04-13 08:55:46 +0700

    revise check price list

--- app/models/price_list.rb
+++ app/models/price_list.rb
@@ -8,6 +8,12 @@ class PriceList < ActiveRecord::Base
     :class_name => 'GoodsHeritage',
     :dependent => :destroy
   has_many :predators, :through => :escapes
+  has_paper_trail
+  mount_uploader :image, PriceListUploader, :mount_on => :image_filename
+
+  scope :find_kategori, lambda {|kat| where(jenis: kat) if kat.present?}
+  scope :find_tipe, lambda {|tipe| where(produk: tipe) if tipe.present?}
+  scope :find_kain, lambda {|kain| where(kain: kain) if kain.present?}
 
   def self.future_to_price_list
     FuturePriceList.where("DATE(updated_at) = ?", Date.today).each do |fpl|
@@ -71,44 +77,48 @@ class PriceList < ActiveRecord::Base
           Cabang.find(lap.cabang_id).regional.each do |cabreg|
             PriceList.where("brand_id = ? and regional_id = ? and kode_barang like ?",
               merk.first.id, cabreg.id, lap.kodebrg).each do |pricelist|
-              customer_services = CheckedItemMaster.where("tanggal = ? and nofaktur like ? and kodebarang like ? and customer like ? and quantity like ?",
-                lap.tanggalsj, lap.nofaktur, lap.kodebrg, lap.customer, lap.jumlah)
+              customer_services = CheckedItemMaster.where("tanggal = ? and nofaktur like ? and kodebarang like ? and customer like ? and quantity like ? and no_so like ?",
+                lap.tanggalsj, lap.nofaktur, lap.kodebrg, lap.customer, lap.jumlah, lap.noso)
               if customer_services.empty?
                 if lap.tanggalsj.month == bulan
                   if (lap.hargasatuan != pricelist.harga) || ((lap.nupgrade*lap.jumlah) != pricelist.upgrade) ||
                       (lap.cashback != pricelist.cashback) || (lap.diskon1 != pricelist.discount_1) ||
                       (lap.diskon2 != pricelist.discount_2) || (lap.diskon3 != pricelist.discount_3) ||
                       (lap.diskon4 != pricelist.discount_4)
-                    CheckedItemMaster.create(:nofaktur => lap.nofaktur, :tgl_faktur => lap.tanggal,
-                      :kodebarang => lap.kodebrg, :namabarang => lap.namabrg, :cabang_id => lap.cabang_id,
-                      :harga_master => pricelist.harga, :harga_laporan => lap.hargasatuan,
-                      :upgrade_master => pricelist.upgrade, :upgrade_laporan => lap.nupgrade,
-                      :panjang => lap.panjang, :lebar => lap.lebar, :netto2 => lap.harganetto2,
-                      :quantity => lap.jumlah, :checked => false, :bonus => lap.bonus,
-                      :cashback_master => pricelist.cashback, :cashback_laporan => lap.cashback,
-                      :discount_1_master => pricelist.discount_1, :discount_1_laporan => lap.diskon1,
-                      :discount_2_master => pricelist.discount_2, :discount_2_laporan => lap.diskon2,
-                      :discount_3_master => pricelist.discount_3, :discount_3_laporan => lap.diskon3,
-                      :discount_4_master => pricelist.discount_4, :discount_4_laporan => lap.diskon4,
-                      :customer => lap.customer, :tanggal => lap.tanggalsj)
+                    LaporanCabang.where(nofaktur: lap.nofaktur).each do |noso|
+                      CheckedItemMaster.create(:nofaktur => noso.nofaktur, :tgl_faktur => noso.tanggal,
+                        :kodebarang => noso.kodebrg, :namabarang => noso.namabrg, :cabang_id => noso.cabang_id,
+                        :harga_master => pricelist.harga, :harga_laporan => noso.hargasatuan,
+                        :upgrade_master => pricelist.upgrade, :upgrade_laporan => noso.nupgrade,
+                        :panjang => noso.panjang, :lebar => noso.lebar, :netto2 => noso.harganetto2,
+                        :quantity => noso.jumlah, :checked => false, :bonus => noso.bonus,
+                        :cashback_master => pricelist.cashback, :cashback_laporan => noso.cashback,
+                        :discount_1_master => pricelist.discount_1, :discount_1_laporan => noso.diskon1,
+                        :discount_2_master => pricelist.discount_2, :discount_2_laporan => noso.diskon2,
+                        :discount_3_master => pricelist.discount_3, :discount_3_laporan => noso.diskon3,
+                        :discount_4_master => pricelist.discount_4, :discount_4_laporan => noso.diskon4,
+                        :customer => noso.customer, :tanggal => noso.tanggalsj, :no_so => noso.noso)
+                    end
                   end
                 else
                   if (lap.hargasatuan != pricelist.prev_harga) || ((lap.nupgrade*lap.jumlah) != pricelist.prev_upgrade) ||
                       (lap.cashback != pricelist.prev_cashback) || (lap.diskon1 != pricelist.prev_discount_1) ||
                       (lap.diskon2 != pricelist.prev_discount_2) || (lap.diskon3 != pricelist.prev_discount_3) ||
                       (lap.diskon4 != pricelist.prev_discount_4)
-                    CheckedItemMaster.create(:nofaktur => lap.nofaktur, :tgl_faktur => lap.tanggal,
-                      :kodebarang => lap.kodebrg, :namabarang => lap.namabrg, :cabang_id => lap.cabang_id,
-                      :harga_master => pricelist.prev_harga, :harga_laporan => lap.hargasatuan,
-                      :upgrade_master => pricelist.prev_upgrade, :upgrade_laporan => lap.nupgrade,
-                      :panjang => lap.panjang, :lebar => lap.lebar, :netto2 => lap.harganetto2,
-                      :quantity => lap.jumlah, :checked => false, :bonus => lap.bonus,
-                      :cashback_master => pricelist.prev_cashback, :cashback_laporan => lap.cashback,
-                      :discount_1_master => pricelist.prev_discount_1, :discount_1_laporan => lap.diskon1,
-                      :discount_2_master => pricelist.prev_discount_2, :discount_2_laporan => lap.diskon2,
-                      :discount_3_master => pricelist.prev_discount_3, :discount_3_laporan => lap.diskon3,
-                      :discount_4_master => pricelist.prev_discount_4, :discount_4_laporan => lap.diskon4,
-                      :customer => lap.customer, :tanggal => lap.tanggalsj)
+                    LaporanCabang.where(nofaktur: lap.nofaktur).each do |noso|
+                      CheckedItemMaster.create(:nofaktur => noso.nofaktur, :tgl_faktur => noso.tanggal,
+                        :kodebarang => noso.kodebrg, :namabarang => noso.namabrg, :cabang_id => noso.cabang_id,
+                        :harga_master => pricelist.prev_harga, :harga_laporan => noso.hargasatuan,
+                        :upgrade_master => pricelist.prev_upgrade, :upgrade_laporan => noso.nupgrade,
+                        :panjang => noso.panjang, :lebar => noso.lebar, :netto2 => noso.harganetto2,
+                        :quantity => noso.jumlah, :checked => false, :bonus => noso.bonus,
+                        :cashback_master => pricelist.prev_cashback, :cashback_laporan => noso.cashback,
+                        :discount_1_master => pricelist.prev_discount_1, :discount_1_laporan => noso.diskon1,
+                        :discount_2_master => pricelist.prev_discount_2, :discount_2_laporan => noso.diskon2,
+                        :discount_3_master => pricelist.prev_discount_3, :discount_3_laporan => noso.diskon3,
+                        :discount_4_master => pricelist.prev_discount_4, :discount_4_laporan => noso.diskon4,
+                        :customer => noso.customer, :tanggal => noso.tanggalsj, :no_so => noso.noso)
+                    end
                   end
                 end
               else
@@ -129,62 +139,6 @@ class PriceList < ActiveRecord::Base
       end
     end
   end
-
-  #  def self.check_lady_test
-  #    LaporanCabang.where("month(tanggalsj) = ? and year(tanggalsj) = ? and kodebrg not like ? and jenisbrgdisc like ?",
-  #      11, 2014, %(___________#{'T'}%), "Lady Americana").no_pengajuan.not_equal_with_nosj.each do |lap|
-  #      unless lap.kodebrg[2].nil?
-  #        merk = Merk.where("IdMerk like ?", "#{lap.kodebrg[2]}")
-  #        unless merk.empty?
-  #          Cabang.find(lap.cabang_id).regional.each do |cabreg|
-  #            PriceList.where("brand_id = ? and regional_id = ? and kode_barang like ?",
-  #              merk.first.id, cabreg.id, lap.kodebrg).each do |pricelist|
-  #              customer_services = CheckedItemMaster.where("tanggal = ? and nofaktur like ? and kodebarang like ? and customer like ? and quantity like ?",
-  #                lap.tanggalsj, lap.nofaktur, lap.kodebrg, lap.customer, lap.jumlah)
-  #              if customer_services.empty?
-  #                if pricelist.brand_id == 4 && pricelist.additional_program == true
-  #                  get_nosj = LaporanCabang.where('nosj like ? and kodejenis in (?) and harganetto1 = ?', lap.nosj, ['HB','DV'], 0)
-  #                  get_nosj.each do |lapcabfree|
-  #                    goods = GoodsHeritage.where("child_kodebarang = ? and parent_kodebarang = ? and program_starting_at >= ?", lapcabfree.kodebrg, pricelist.kode_barang, lap.tanggalsj)
-  #                    if goods.empty?
-  #                      unless pricelist.parent_items.find_by_child_kodebarang(lapcabfree.kodebrg).nil?
-  #                        if lap.diskon4 != pricelist.parent_items.find_by_child_kodebarang(lapcabfree.kodebrg).diskon4
-  #                          CheckedItemMaster.create(:nofaktur => lap.nofaktur, :tgl_faktur => lap.tanggal,
-  #                            :kodebarang => lap.kodebrg, :namabarang => lap.namabrg, :cabang_id => lap.cabang_id,
-  #                            :harga_master => pricelist.harga, :harga_laporan => lap.hargasatuan,
-  #                            :upgrade_master => pricelist.upgrade, :upgrade_laporan => lap.nupgrade,
-  #                            :panjang => lap.panjang, :lebar => lap.lebar,
-  #                            :quantity => lap.jumlah, :checked => false,
-  #                            :cashback_master => pricelist.cashback, :cashback_laporan => lap.cashback,
-  #                            :discount_1_master => pricelist.discount_1, :discount_1_laporan => lap.diskon1,
-  #                            :discount_2_master => pricelist.discount_2, :discount_2_laporan => lap.diskon2,
-  #                            :discount_3_master => pricelist.discount_3, :discount_3_laporan => lap.diskon3,
-  #                            :discount_4_master => pricelist.discount_4, :discount_4_laporan => lap.diskon4,
-  #                            :customer => lap.customer, :tanggal => lap.tanggalsj)
-  #                          CheckedItemMaster.create(:nofaktur => lap.nofaktur, :tgl_faktur => lap.tanggal,
-  #                            :kodebarang => lapcabfree.kodebrg, :namabarang => lap.namabrg, :cabang_id => lap.cabang_id,
-  #                            :harga_master => pricelist.harga, :harga_laporan => lap.hargasatuan,
-  #                            :upgrade_master => pricelist.upgrade, :upgrade_laporan => lap.nupgrade,
-  #                            :panjang => lap.panjang, :lebar => lap.lebar,
-  #                            :quantity => lap.jumlah, :checked => false,
-  #                            :cashback_master => pricelist.cashback, :cashback_laporan => lap.cashback,
-  #                            :discount_1_master => pricelist.discount_1, :discount_1_laporan => lap.diskon1,
-  #                            :discount_2_master => pricelist.discount_2, :discount_2_laporan => lap.diskon2,
-  #                            :discount_3_master => pricelist.discount_3, :discount_3_laporan => lap.diskon3,
-  #                            :discount_4_master => pricelist.discount_4, :discount_4_laporan => lap.diskon4,
-  #                            :customer => lap.customer, :tanggal => lap.tanggalsj)
-  #                        end
-  #                      end
-  #                    end
-  #                  end
-  #                end
-  #              end
-  #            end
-  #          end
-  #        end
-  #      end
-  #    end
-  #  end
 
   def self.future_to_price
     FuturePriceList.all.each do |fpl|
