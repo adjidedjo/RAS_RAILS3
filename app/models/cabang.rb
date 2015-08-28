222ee02e74c81c736e0744dbf0e6509bb127b3df
adjidedjo <adji.el.djo@gmail.com>
2015-08-28 10:13:35 +0700

    revising price list

--- app/models/cabang.rb
+++ app/models/cabang.rb
@@ -7,26 +7,22 @@ class Cabang < ActiveRecord::Base
   has_many :yearly_target
   has_many :regional, :through => :regional_branches
   has_many :regional_branches
-  
+
   scope :branch_name, lambda {|branch| where(:id => branch)}
-  
+
   def self.branch_get_name(current_user)
     current_user.branch == nil ? get_id : branch_name(current_user.branch)
   end
 
   def self.get_id
-    find(2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 18, 19, 20, 22, 23, 24)
-  end
-  
-  def self.get_id
-    find(2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 18, 19, 20, 22, 23, 24)
+    find(1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 18, 19, 20, 22, 23, 24)
   end
 
   def self.get_id_to_7
-    find(2, 3, 4, 5, 7, 8, 9, 10)
+    find(1, 2, 3, 4, 5, 7, 8, 9, 10)
   end
 
-	def self.get_id_to_22
+  def self.get_id_to_22
     find(11, 13, 18, 19, 20, 22, 23, 24)
   end
 
