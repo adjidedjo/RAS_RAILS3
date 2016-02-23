f50eb7ef69f7a0b93b40aaa949f984cb881bf6f9
adjidedjo <adji.el.djo@gmail.com>
2016-02-23 15:18:13 +0700

    revising brand for jde

--- app/helpers/accounting_price_list_helper.rb
+++ app/helpers/accounting_price_list_helper.rb
@@ -1,6 +1,6 @@
 module AccountingPriceListHelper
   def options_for_tipe_price
-    ["ELITE(JDE)", "Non Serenity", "Serenity", "LADY(JDE)", "Lady Americana", "Royal", "Classic", "Grand",
+    ["ELITE", "Non Serenity", "Serenity", "LADY", "Lady Americana", "Royal", "Classic", "Grand",
       "Clearance Sale"
     ]
   end
