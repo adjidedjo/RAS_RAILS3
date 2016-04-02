324531418c3e1900ecc1aceaba7c78eb7c86cdb3
adjidedjo <adji.el.djo@gmail.com>
2016-04-02 08:22:48 +0700

    revising auto intransit

--- app/models/pos_auto_intransit.rb
+++ app/models/pos_auto_intransit.rb
@@ -4,14 +4,17 @@ class PosAutoIntransit < ActiveRecord::Base
 
   def self.insert_delivered_stock_from_jde
     PosChannelCustomer.where("address_number > ?", 0).each do |pcc|
-      SalesOrderHistoryJde.find_sales_transfer_to_showroom(Date.today, pcc.address_number).each do |soh|
+      SalesOrderHistoryJde.find_sales_transfer_to_showroom("16/03/2016".to_date, pcc.address_number).each do |soh|
         jde_date_today = jde_date_to_date(soh.sdaddj.to_i)
         nama_brg = soh.sddsc1.strip + " " + soh.sddsc2.strip
         duplicate_stock = self.where(channel_customer_id: pcc.id, serial: soh.sdlotn, kode_barang: soh.sdaitm.strip, no_sj: soh.sddeln)
         if duplicate_stock.blank?
-          self.create(channel_customer_id: pcc.id, serial: soh.sdlotn.strip, kode_barang: soh.sdaitm, no_sj: soh.sddeln.to_i, nama: nama_brg,
-            jumlah: soh.sdsoqs.to_i.to_s[0..-5], stok_awal: soh.sdsoqs.to_i.to_s[0..-5], no_so: soh.sddoco.to_i, tanggal_sj: jde_date_today,
-            checked_in: 1, checked_in_by: 1)
+          self.create(channel_customer_id: pcc.id, serial: soh.sdlotn.strip, kode_barang: soh.sdaitm.strip, no_sj: soh.sddeln.to_i, nama: nama_brg,
+            jumlah: soh.sdsoqs.to_i.to_s[0..-5], stok_awal: soh.sdsoqs.to_i.to_s[0..-5], no_so: soh.sddoco.to_i, tanggal_sj: jde_date_today)
+          item_masters = PosItemMaster.find_by_kode_barang(soh.sdaitm.strip)
+          if item_masters.present?
+            item_masters.update_attributes!(nama: nama_brg)
+          end
         end
       end
     end
