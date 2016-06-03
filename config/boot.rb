27d9e9dfa3bcd6aa2bc6cbeae284ddaaa69ec50d
adjidedjo <adji.el.djo@gmail.com>
2016-06-03 13:20:08 +0700

    add yaml

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 
+require 'yaml'
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+YAML::ENGINE.yamler= 'syck'
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
