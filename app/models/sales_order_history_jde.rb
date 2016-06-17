1d63d927ad50b444f56e3981f83e5cec02db4551
adjidedjo <adji.el.djo@gmail.com>
2016-06-17 11:10:57 +0700

    revise auto intransit jde

--- app/models/sales_order_history_jde.rb
+++ app/models/sales_order_history_jde.rb
@@ -3,7 +3,7 @@ class SalesOrderHistoryJde < ActiveRecord::Base
   establish_connection "jdeoracle"
   self.table_name = "proddta.f42119" #sd
 
-  scope :delivered, -> { where("sdnxtr like ? and sdlttr like ? and sddcto like ?", "999", "580", "ST") }
+  scope :delivered, -> { where("sdnxtr like ? and sdlttr like ? and sddcto like ?", "580", "565", "ST") }
 
   def self.find_sales_transfer_to_showroom(date, showroom_id)
     where("sdaddj = ? and sdshan like ?", date_to_julian(date), showroom_id).delivered
