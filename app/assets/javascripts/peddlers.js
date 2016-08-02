b1b8aed23a12b74e8546f244bf06c9ba34bcad83
adjidedjo <adji.el.djo@gmail.com>
2016-08-02 09:19:51 +0700

    revise asongan

---
+++ app/assets/javascripts/peddlers.js
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+  $('#test').dataTable({
+    sPaginationType: "full_numbers",
+    iDisplayLength: 10
+  });
+});
