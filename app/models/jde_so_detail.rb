e79e47e4e2de7786a6bdbbd113dc3caf09193e64
adjidedjo <adji.el.djo@gmail.com>
2017-01-26 10:05:13 +0700

    add sales display and clearence branch

--- app/models/jde_so_detail.rb
+++ app/models/jde_so_detail.rb
@@ -44,6 +44,38 @@ class JdeSoDetail < ActiveRecord::Base
     end
   end
 
+  #use this if sales of a branch not working
+  def self.import_so_detail_with_range(branch)
+    where("sdnxtr >= ? and sdlttr >= ? and sddcto = ? and sdaddj BETWEEN ? and ?",
+    "580", "565", "SO", date_to_julian("01/01/2017".to_date), date_to_julian(Date.today)).each do |a|
+      find_sj = LaporanCabang.where(nosj: a.sddeln.to_i, lnid: a.sdlnid.to_i)
+      if find_sj.empty?
+        fullnamabarang = "#{a.sddsc1.strip} " "#{a.sddsc2.strip}"
+        customer = JdeCustomerMaster.find_by_aban8(a.sdan8)
+        bonus = a.sdaexp == 0 ?  'BONUS' : '-'
+        if customer.abat1.strip == "C"
+          namacustomer = customer.abalph.strip
+          cabang = jde_cabang(branch)
+          item_master = JdeItemMaster.find_by_imitm(a.sditm)
+          jenis = JdeUdc.jenis_udc(item_master.imseg1.strip)
+          artikel = JdeUdc.artikel_udc(item_master.imseg2.strip)
+          kain = JdeUdc.kain_udc(item_master.imseg3.strip)
+          groupitem = JdeUdc.group_item_udc(a.sdsrp3.strip)
+          harga = JdeBasePrice.harga_satuan(a.sditm, a.sdmcu.strip, a.sdtrdj)
+          kota = JdeAddressByDate.get_city(a.sdan8.to_i)
+          group = JdeCustomerMaster.get_group_customer(a.sdan8.to_i)
+         sales = JdeSalesman.find_salesman(a.sdan8.to_i, a.sdsrp1.strip)
+          LaporanCabang.create(cabang_id: cabang, noso: a.sddoco.to_i, nosj: a.sddeln.to_i, tanggalsj: julian_to_date(a.sdaddj),kodebrg: a.sdaitm.strip,
+            namabrg: fullnamabarang, kode_customer: a.sdan8.to_i, customer: namacustomer, jumlah: a.sdsoqs.to_s.gsub(/0/,"").to_i, satuan: "PC",
+            jenisbrgdisc: item_master.imprgr.strip, kodejenis: item_master.imseg1.strip, jenisbrg: jenis, kodeartikel: item_master.imaitm[2..5], namaartikel: artikel,
+            kodekain: item_master.imseg3.strip, namakain: kain, panjang: item_master.imseg5.to_i, lebar: item_master.imseg6.to_i, namabrand: groupitem,
+            hargasatuan: harga/10000, harganetto1: a.sdaexp, harganetto2: a.sdaexp, kota: kota, tipecust: group, bonus: bonus, lnid: a.sdlnid.to_i, ketppb: "",
+            salesman: sales)
+        end
+      end
+    end
+  end
+
   private
   def self.date_to_julian(date)
     1000*(date.year-1900)+date.yday
@@ -56,21 +88,21 @@ class JdeSoDetail < ActiveRecord::Base
   def self.jde_cabang(bu)
     if bu == "11001" || bu == "11002" #pusat
       "01"
-    elsif bu == "11011" || bu == "11012" #jabar
+    elsif bu == "11011" || bu == "11012" || bu == "11011C" || bu == "11011D" #jabar
       "02"
-    elsif bu == "11021" || bu == "11022" #cirebon
+    elsif bu == "11021" || bu == "11022" || bu == "13021" || bu == "13021D" || bu == "13021C" || bu == "11021C" || bu == "11021D" #cirebon
       "09"
-    elsif bu == "12061" || bu == "12062" || bu == "12001" #bestari mulia
+    elsif bu == "12061" || bu == "12062" || bu == "12001" || bu == "12061C" || bu == "12061D" #bestari mulia
       "07"
-    elsif bu == "11151" || bu == "11152" #cikupa
+    elsif bu == "11151" || bu == "11152" || bu == "13151" || bu == "13151C" || bu == "13151D" || bu == "11151C" || bu == "11151D" #cikupa
       "23"
-    elsif bu == "11031" || bu == "11032" #narogong
+    elsif bu == "11031" || bu == "11032" || bu == "11031C" || bu == "11031D" #narogong
       "03"
-    elsif bu == "12111" || bu == "12112" #makasar
+    elsif bu == "12111" || bu == "12112" || bu == "13111" || bu == "12111C" || bu == "12111D" || bu == "13111C" || bu == "13111D" #makasar
       "19"
-    elsif bu == "12071" || bu == "12072" #bali
+    elsif bu == "12071" || bu == "12072" || bu == "13071" || bu == "12071C" || bu == "12071D" || bu == "13071C" || bu == "13071D" #bali
       "04"
-    elsif bu == "12131" || bu == "12132" #jember
+    elsif bu == "12131" || bu == "12132" || bu == "13131" || bu == "12131C" || bu == "12131D" || bu == "13131C" || bu == "13131D" #jember
       "22"
     end
   end
