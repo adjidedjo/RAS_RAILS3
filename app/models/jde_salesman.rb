2abf97cb831a0f8fa25b66387a470cf0bca7eb99
adjidedjo <adji.el.djo@gmail.com>
2017-03-18 09:29:35 +0700

    add salesman id

--- app/models/jde_salesman.rb
+++ app/models/jde_salesman.rb
@@ -2,12 +2,18 @@ class JdeSalesman < ActiveRecord::Base
   establish_connection "jdeoracle"
   self.table_name = "proddta.f40344" #sa
   
-  
   def self.find_salesman(customer_id, brand)
     commision_table = find_by_sql("SELECT saslsm FROM proddta.f40344 WHERE saan8 like '%#{customer_id}%' 
     AND sait44 like '#{brand}%' AND saexdj > '#{date_to_julian(Date.today.to_date)}'")
     salesman_name = commision_table.present? ? JdeCustomerMaster.find_salesman_name(commision_table.first.saslsm.to_i) : '-'
     return salesman_name
+  end
+  
+  def self.find_salesman_id(customer_id, brand)
+    commision_table = find_by_sql("SELECT saslsm FROM proddta.f40344 WHERE saan8 like '%#{customer_id}%' 
+    AND sait44 like '#{brand}%' AND saexdj > '#{date_to_julian(Date.today.to_date)}'")
+    salesman_id = commision_table.present? ? commision_table.first.saslsm.to_i : '-'
+    return salesman_id
   end
 
   def self.date_to_julian(date)
--- app/models/jde_so_detail.rb
+++ app/models/jde_so_detail.rb
@@ -34,12 +34,13 @@ class JdeSoDetail < ActiveRecord::Base
           group = JdeCustomerMaster.get_group_customer(a.sdan8.to_i)
           variance = (julian_to_date(a.sdaddj)-julian_to_date(a.sddrqj)).to_i
          sales = JdeSalesman.find_salesman(a.sdan8.to_i, a.sdsrp1.strip)
+         sales_id = JdeSalesman.find_salesman_id(a.sdan8.to_i, a.sdsrp1.strip)
           LaporanCabang.create(cabang_id: cabang, noso: a.sddoco.to_i, tanggal: julian_to_date(a.sdtrdj), nosj: a.sddeln.to_i, tanggalsj: julian_to_date(a.sdaddj),kodebrg: a.sdaitm.strip,
             namabrg: fullnamabarang, kode_customer: a.sdan8.to_i, customer: namacustomer, jumlah: a.sdsoqs.to_s.gsub(/0/,"").to_i, satuan: "PC",
             jenisbrgdisc: item_master.imprgr.strip, kodejenis: item_master.imseg1.strip, jenisbrg: jenis, kodeartikel: item_master.imaitm[2..5], namaartikel: artikel,
             kodekain: item_master.imseg3.strip, namakain: kain, panjang: item_master.imseg5.to_i, lebar: item_master.imseg6.to_i, namabrand: groupitem,
             hargasatuan: harga/10000, harganetto1: a.sdaexp, harganetto2: a.sdaexp, kota: kota, tipecust: group, bonus: bonus, lnid: a.sdlnid.to_i, ketppb: "",
-            salesman: sales, diskon5: variance, orty: a.sddcto.strip)
+            salesman: sales, diskon5: variance, orty: a.sddcto.strip, nopo: sales_id)
         end
       end
     end
