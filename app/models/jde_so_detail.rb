eba7f5e33b319c6ecaa7e174db5a265ac47dc678
adjidedjo <adji.el.djo@gmail.com>
2017-03-29 19:41:51 +0700

    change batch time for credit note

--- app/models/jde_so_detail.rb
+++ app/models/jde_so_detail.rb
@@ -82,7 +82,7 @@ class JdeSoDetail < ActiveRecord::Base
   #import credit note
   def self.import_credit_note
     credit_note = self.find_by_sql("SELECT * FROM PRODDTA.F03B11 WHERE rpfy = 
-    '%17%' AND rpdct LIKE '%RM%'")
+    '%17%' AND rpdct LIKE '%RM%' AND rprmr1 NOT LIKE '%RO%'")
     credit_note.each do |cr|
       no_doc = cr.rprmk[0..7].to_i.to_s
       no_so = self.find_by_sql("SELECT sdtrdj, sdan8, sdmcu, sddoco, sddeln, sdsrp1
