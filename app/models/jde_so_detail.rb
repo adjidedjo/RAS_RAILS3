d66337e96733f2d8b2d2748b3fb5973fff6c8f60
adjidedjo <adji.el.djo@gmail.com>
2017-04-18 22:05:21 +0700

    fix acc receivable batch

--- app/models/jde_so_detail.rb
+++ app/models/jde_so_detail.rb
@@ -186,7 +186,8 @@ class JdeSoDetail < ActiveRecord::Base
   
   # import account receivable
   def self.import_acc_receivable
-    ar = self.find_by_sql("SELECT * FROM PRODDTA.F03B11 WHERE rpupmj ='#{date_to_julian(Date.yesterday.to_date)}'")
+    ar = self.find_by_sql("SELECT * FROM PRODDTA.F03B11 WHERE rpupmj BETWEEN 
+    '#{date_to_julian('01/03/2017'.to_date)}' AND '#{date_to_julian('17/04/2017'.to_date)}'")
     ar.each do |ars|
       cek_ava = AccountReceivable.where(doc_number: ars.rpdoc, doc_type: ars.rpdct, branch: ars.rpmcu.strip, pay_item: ars.rpsfx)
       if cek_ava.empty?
