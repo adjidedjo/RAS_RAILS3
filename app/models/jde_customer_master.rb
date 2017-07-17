d0aed877f800820c690b61339cc06c68f8dc3ee4
adjidedjo <adji.el.djo@gmail.com>
2017-07-17 13:35:57 +0700

    fix import credit limit

--- app/models/jde_customer_master.rb
+++ app/models/jde_customer_master.rb
@@ -64,7 +64,7 @@ class JdeCustomerMaster < ActiveRecord::Base
         SELECT SUM(rpaap) AS rpag, rpan8, rpkco, MAX(rpmcu) AS rpmcu FROM PRODDTA.F03B11 WHERE rppst NOT LIKE '%P%'
         GROUP BY rpan8, rpkco ORDER BY rpmcu
       ) RP ON RP.rpkco = AI.aico AND RP.rpan8 = AB.aban8
-      WHERE AI.aico > 0 AND AB.absic LIKE '%RET%' 
+      WHERE AI.aico > 0 AND AB.absic LIKE '%RET%' AND AI.aian8 LIKE '%102095%'
       GROUP BY AI.aiacl, AI.aidaoj, AI.aian8, AB.abalph, AB.absic, AL.alcty1, AI.aicusts, AB.abmcu, 
       AB.absic, AI.aico, RP.rpag, AI.aiaprc, RP.rpmcu
     ")
@@ -74,11 +74,12 @@ class JdeCustomerMaster < ActiveRecord::Base
         CustomerLimit.create!(address_number: nc.aian8, name: nc.abalph.strip, i_class: nc.absic.strip, 
           city: nc.alcty1.nil? ? '-' : nc.alcty1.strip, opened_date: julian_to_date(nc.aidaoj), 
           branch_id: jde_cabang(customer.first.abmcu.strip), 
-          area_id: nc.rpmcu.nil? ? nc.rpmcu : find_area(jde_cabang(nc.rpmcu.strip)), state: customer.first.aicusts, 
+          area_id: nc.rpmcu.nil? ? jde_cabang(customer.first.abmcu.strip) : find_area(jde_cabang(nc.rpmcu.strip)), state: customer.first.aicusts, 
           limit: nc.aiacl.to_i, co: nc.aico, amount_due: nc.rpag, open_amount: nc.aiaprc)
        elsif find_cus.present?
+         raise nc.rpmcu.inspect
          find_cus.first.update_attributes!(state: nc.aicusts, limit: nc.aiacl.to_i, 
-         area_id: nc.rpmcu.nil? ? nc.rpmcu : find_area(jde_cabang(nc.rpmcu.strip)), amount_due: nc.rpag, open_amount: nc.aiaprc)   
+         area_id: nc.rpmcu.nil? ? jde_cabang(customer.first.abmcu.strip) : find_area(jde_cabang(nc.rpmcu.strip)), amount_due: nc.rpag, open_amount: nc.aiaprc)   
       end
     end
   end
@@ -128,37 +129,37 @@ class JdeCustomerMaster < ActiveRecord::Base
   end
 
   def self.jde_cabang(bu)
-    if bu == "11001"|| bu == "11001D" || bu == "11001C" #pusat
+    if bu.include?("1300") || bu.include?("1100") || bu == "11001" || bu == "11001D" || bu == "11001C" #pusat
       "01"
-    elsif bu == "11011" || bu == "11012" || bu == "11011C" || bu == "11011D" || bu == "11002" || bu == "13011D" || bu == "13011" || bu == "13011C" || bu == "11011S" || bu == "13011S" #jabar
+    elsif bu.include?("1301") || bu == "11010" || bu.include?("1101") || bu == "11011C" || bu == "11011D" || bu == "11002" || bu == "13011D" || bu == "13011" || bu == "13011C" || bu == "11011S" || bu == "13011S" #jabar
       "02"
-    elsif bu == "11021" || bu == "11022" || bu == "13021" || bu == "13021D" || bu == "13021C" || bu == "11021C" || bu == "11021D" || bu == "11021S" || bu == "13021S" #cirebon
+    elsif bu.include?("1302") || bu.include?("1102") || bu == "11021" || bu == "11022" || bu == "13021" || bu == "13021D" || bu == "13021C" || bu == "11021C" || bu == "11021D" || bu == "11021S" || bu == "13021S" #cirebon
       "09"
-    elsif bu == "12001" || bu == "12002" || bu == "12001C" || bu == "12001D" #bestari mulia
+    elsif bu.include?("1200") || bu == "12001" || bu == "12002" || bu == "12001C" || bu == "12001D" #bestari mulia
       "50"
-    elsif bu == "12061" || bu == "12062" || bu == "12001" || bu == "12061C" || bu == "12061D" || bu == "13061"|| bu == "13001" || bu == "13061C" || bu == "13061D" || bu == "12061S" || bu == "13061S" #surabay
+    elsif bu.include?("1306") || bu.include?("1206") || bu == "12061" || bu == "12062" || bu == "12001" || bu == "12061C" || bu == "12061D" || bu == "13061"|| bu == "13001" || bu == "13061C" || bu == "13061D" || bu == "12061S" || bu == "13061S" #surabay
       "07"
-    elsif bu == "11151" || bu == "11152" || bu == "13151" || bu == "13151C" || bu == "13151D" || bu == "11151C" || bu == "11151D" || bu == "11151S" || bu == "13151S" #cikupa
+    elsif bu.include?("1315") || bu.include?("1115") || bu == "11151" || bu == "11152" || bu == "13151" || bu == "13151C" || bu == "13151D" || bu == "11151C" || bu == "11151D" || bu == "11151S" || bu == "13151S" #cikupa
       "23"
-    elsif bu == "11031" || bu == "11032" || bu == "11031C" || bu == "11031D" || bu == "13031C" || bu == "13031D" || bu == "13031" || bu == "11031S" || bu == "13031S" #narogong
+    elsif bu.include?("1303") || bu.include?("1103") || bu == "11031" || bu == "11032" || bu == "11031C" || bu == "11031D" || bu == "13031C" || bu == "13031D" || bu == "13031" || bu == "11031S" || bu == "13031S" #narogong
       "03"
-    elsif bu == "12111" || bu == "12112" || bu == "13111" || bu == "12111C" || bu == "12111D" || bu == "13111C" || bu == "13111D" || bu == "12111S" || bu == "13111S" #makasar
+    elsif bu.include?("1311") || bu.include?("1211") || bu == "12111" || bu == "12112" || bu == "13111" || bu == "12111C" || bu == "12111D" || bu == "13111C" || bu == "13111D" || bu == "12111S" || bu == "13111S" #makasar
       "19"
-    elsif bu == "12071" || bu == "12072" || bu == "13071" || bu == "12071C" || bu == "12071D" || bu == "13071C" || bu == "13071D" || bu == "12111S" || bu == "13071S" #bali
+    elsif bu.include?("1307") || bu.include?("1207") || bu == "12071" || bu == "12072" || bu == "13071" || bu == "12071C" || bu == "12071D" || bu == "13071C" || bu == "13071D" || bu == "12111S" || bu == "13071S" #bali
       "04"
-    elsif bu == "12131" || bu == "12132" || bu == "13131" || bu == "12131C" || bu == "12131D" || bu == "13131C" || bu == "13131D" || bu == "12131S" || bu == "13131S" #jember
+    elsif bu.include?("1313") || bu.include?("1213") || bu == "12131" || bu == "12132" || bu == "13131" || bu == "12131C" || bu == "12131D" || bu == "13131C" || bu == "13131D" || bu == "12131S" || bu == "13131S" #jember
       "22"
-    elsif bu == "11101" || bu == "11102" || bu == "13101" || bu == "11101C" || bu == "11101D" || bu == "13101C" || bu == "13101D" || bu == "11101S" || bu == "13101S" #lampung
+    elsif bu.include?("1310") || bu.include?("1110") || bu == "11101" || bu == "11102" || bu == "13101" || bu == "11101C" || bu == "11101D" || bu == "13101C" || bu == "13101D" || bu == "11101S" || bu == "13101S" #lampung
       "13"  
-    elsif bu == "11091" || bu == "11092" || bu == "13091" || bu == "11091C" || bu == "11091D" || bu == "13091C" || bu == "13091D" || bu == "11091S" || bu == "13091S" #palembang
+    elsif bu.include?("1309") || bu.include?("1109") || bu == "11091" || bu == "11092" || bu == "13091" || bu == "11091C" || bu == "11091D" || bu == "13091C" || bu == "13091D" || bu == "11091S" || bu == "13091S" #palembang
       "11"
-    elsif bu == "11041" || bu == "11042" || bu == "13041" || bu == "11041C" || bu == "11041D" || bu == "13041C" || bu == "13041D" || bu == "11041S" || bu == "13041S"#yogyakarta
+    elsif bu.include?("1304") || bu.include?("1104") || bu == "11041" || bu == "11042" || bu == "13041" || bu == "11041C" || bu == "11041D" || bu == "13041C" || bu == "13041D" || bu == "11041S" || bu == "13041S"#yogyakarta
       "10"
-    elsif bu == "11051" || bu == "11052" || bu == "13051" || bu == "11051C" || bu == "11051D" || bu == "13051C" || bu == "13051D" || bu == "11051S" || bu == "13051S" #semarang
+    elsif bu.include?("1305") || bu.include?("1105") || bu == "11051" || bu == "11052" || bu == "13051" || bu == "11051C" || bu == "11051D" || bu == "13051C" || bu == "13051D" || bu == "11051S" || bu == "13051S" #semarang
       "08"
-    elsif bu == "11081" || bu == "11082" || bu == "13081" || bu == "11081C" || bu == "11081D" || bu == "13081C" || bu == "13081D" || bu == "11081S" || bu == "13081S" #medan
+    elsif bu.include?("1308") || bu.include?("1108") || bu == "11081" || bu == "11082" || bu == "13081" || bu == "11081C" || bu == "11081D" || bu == "13081C" || bu == "13081D" || bu == "11081S" || bu == "13081S" #medan
       "05"
-    elsif bu == "11121" || bu == "11122" || bu == "13121" || bu == "11121C" || bu == "11121D" || bu == "13121C" || bu == "13121D" || bu == "11121S" || bu == "13121S" #pekanbaru
+    elsif bu.include?("1312") || bu.include?("1112") || bu == "11121" || bu == "11122" || bu == "13121" || bu == "11121C" || bu == "11121D" || bu == "13121C" || bu == "13121D" || bu == "11121S" || bu == "13121S" #pekanbaru
       "20"
     end
   end
