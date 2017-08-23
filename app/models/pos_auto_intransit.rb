449b79dfc9e102e05f10f1d25c6d154621b9a6dd
adjidedjo <adji.el.djo@gmail.com>
2017-08-23 08:31:47 +0700

    fix import stock from jde for pos

--- app/models/pos_auto_intransit.rb
+++ app/models/pos_auto_intransit.rb
@@ -8,7 +8,7 @@ class PosAutoIntransit < ActiveRecord::Base
         stocking_type = (soh.sdmcu.include? "D") ? "RE" : "CS"
         jde_date_today = jde_date_to_date(soh.sdaddj.to_i)
         nama_brg = soh.sddsc1.strip + " " + soh.sddsc2.strip
-        duplicate_stock = self.where(channel_customer_id: pcc.id, serial: soh.sdlotn, kode_barang: soh.sdaitm.strip, no_sj: soh.sddeln)
+        duplicate_stock = self.where(channel_customer_id: pcc.id, serial: soh.sdlotn, kode_barang: soh.sdlitm.strip, no_sj: soh.sddeln)
         if duplicate_stock.blank?
           self.create(channel_customer_id: pcc.id, serial: soh.sdlotn.strip, kode_barang: soh.sdlitm.strip, no_sj: soh.sddeln.to_i, nama: nama_brg,
             jumlah: soh.sdsoqs.to_i.to_s[0..-5], stok_awal: soh.sdsoqs.to_i.to_s[0..-5], no_so: soh.sddoco.to_i, tanggal_sj: jde_date_today, stocking_type: stocking_type)
