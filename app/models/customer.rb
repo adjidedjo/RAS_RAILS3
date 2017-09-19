9d1e7db51e667a96a48918ed138a6a46672ffc51
adjidedjo <adji.el.djo@gmail.com>
2017-09-19 09:08:23 +0700

    fix user mailer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,22 @@ class Customer < ActiveRecord::Base
   scope :customer, lambda {|customer| where("nama_customer in (?)", customer) if customer.present? }
   scope :customer_channel, lambda {|channel| where("tipe_customer in (?)", channel) if channel.present?}
   scope :customer_group, lambda {|group| where("group_customer in (?)", group) if group.present?}
+  
+  def self.notification_order
+    order_jde = JdeSoHeader.get_order_today
+    order_jde.each do |oje|
+      find_by_sql("SELECT * FROM jde_customers WHERE address_number = '#{oje.shan8.to_i}'").each do |c|
+        unless c.email.nil?
+          JdeSoDetail.get_list_of_item(oje.shdoco, oje.shdcto)
+          raise oje.inspect
+          UserMailer.order(c.email, c.name, oje.oamlnm, oje.oaadd1, oje.oaadd2, oje.oacty1, 
+          JdeSoHeader.julian_to_date(oje.shdrqj), JdeSoDetail.get_list_of_item(oje.shdoco, oje.shdcto), oje.shdoco,
+          oje.alcty1, oje.aladd1).deliver
+          c.update_attributes!(date_mailed: Date.today)
+        end
+      end
+    end
+  end
 
   def self.sales_cabang_per_toko(cabang, date)
     select("*")
