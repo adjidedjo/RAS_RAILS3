d29b5c01970f81e4aa3bea00c99d0645f25752a0
adjidedjo <adji.el.djo@gmail.com>
2017-11-24 08:50:24 +0700

    fix item master fetch

--- app/models/jde_item_master.rb
+++ app/models/jde_item_master.rb
@@ -8,12 +8,21 @@ class JdeItemMaster < ActiveRecord::Base
     where(imitm: short_item)
   end
 
+  def self.update_second
+    PosItemMaster.all.each do |pim|
+      where("imlitm LIKE '%#{pim.kode_barang}%'").each do |imjde|
+        pim.update_attributes!(jde_item_master: imjde.imaitm.strip)
+      end
+    end
+  end
+  
+
   def self.get_new_items_from_jde
     where("imtmpl like ? and imseg4 = ? and imupmj like ?", "%BJ MATRASS%", "S", date_to_julian(Date.yesterday.to_date)).include_items.each do |imjde|
       pim = PosItemMaster.where("kode_barang like ?", imjde.imaitm.strip)
       if pim.empty?
         nama_brg = imjde.imdsc1.strip + " " + imjde.imdsc2.strip
-        PosItemMaster.create(kode_barang: imjde.imaitm.strip, nama: nama_brg, brand_id: brand(imjde.imprgr.strip), jenis: imjde.imseg1.strip, harga: 0)
+        PosItemMaster.create(kode_barang: imjde.imlitm.strip, nama: nama_brg, brand_id: brand(imjde.imprgr.strip), jenis: imjde.imseg1.strip, harga: 0)
       end
     end
   end
@@ -21,12 +30,13 @@ class JdeItemMaster < ActiveRecord::Base
   def self.item_masters_fetch
     item_masters = find_by_sql("SELECT IMITM, IMLITM, IMDSC1, IMDSC2, IMSRP1, IMSRP3, IMSRP9, IMPRGR, IMTMPL, IMSEG1,
     IMSEG2, IMSEG3, IMSEG4, IMSEG5, IMSEG6, IMSEG7, IMSEG8, IMSEG9, IMSEG0 FROM PRODDTA.F4101 WHERE
-    IMTMPL LIKE '%BJMATRASS%' AND IMUPMJ LIKE '#{date_to_julian(Date.yesterday.to_date)}'")
+    IMTMPL LIKE '%BJ MATRASS%' AND IMSEG4 LIKE '%S%' 
+    AND IMUPMJ = '#{date_to_julian(Date.yesterday.to_date)}'")
     item_masters.each do |im|
       check_item = ItemMaster.find_by_short_item_no(im.imitm)
-      if check_item.empty? || check_item.nil?
-        ItemMaster.create(short_item_no: im.imitm, item_number: im.imlitm, desc: im.desc1.strip, 
-        desc2: im.desc2.strip, slscd1: im.imsrp1, slscd2: im.imsrp3, catcd9: im.imsrp9, 
+      if check_item.nil? || check_item.nil?
+        ItemMaster.create(short_item_no: im.imitm, item_number: im.imlitm, desc: im.imdsc1.strip, 
+        desc2: im.imdsc2.strip, slscd1: im.imsrp1, slscd2: im.imsrp3, catcd9: im.imsrp9, 
         group: im.imprgr.strip, template: im.imtmpl, segment1: im.imseg1, segment2: im.imseg2, 
         segment3: im.imseg3, segment4: im.imseg4, segment5: im.imseg5, segment6: im.imseg6, 
         segment7: im.imseg7, segment8: im.imseg8, segment9: im.imseg9, segment10: im.imseg0)
