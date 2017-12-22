841dbcd14fb51241983e0be21dd23da8d8dd7205
adjidedjo <adji.el.djo@gmail.com>
2017-12-22 10:50:12 +0700

    fix report for asong

--- app/models/asong_order.rb
+++ app/models/asong_order.rb
@@ -1,15 +1,14 @@
 class AsongOrder < ActiveRecord::Base
   after_create :creating_point
   def creating_point
-    check_stock = checking_stock(self.short_item, self.branch_plant)
+    check_stock = checking_stock(self.short_item, self.branch_plant).map(&:lilotn)
     aln = AsongLotNumber.find_by_sql("SELECT lot_number FROM asong_lot_numbers WHERE
-    short_item = '#{self.short_item}' and branch = '#{self.branch_plant}'")
-    check_stock = check_stock -= aln
-    choosen_lot = []
+    short_item = '#{self.short_item}' and branch = '#{self.branch_plant}'").map(&:lot_number)
+    check_stock -= aln
     if check_stock.present?
       val_params = check_stock.count < self.quantity.to_i ? check_stock : check_stock.first(self.quantity.to_i)
       val_params.each do |a|
-        al = AsongLotNumber.create!(asong_order_id: self.id, lot_number: a.lilotn, branch: self.branch_plant,
+        al = AsongLotNumber.create!(asong_order_id: self.id, lot_number: a, branch: self.branch_plant,
         short_item: self.short_item)
         AsongPoint.create!(asong_order_id: self.id, branch: self.branch, brand: self.brand, point: 1)
       end
