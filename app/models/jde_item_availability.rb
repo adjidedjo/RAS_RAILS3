99cf0da5ea8bb4a7b416f42d8f91d2b6173f29dc
adjidedjo <adji.el.djo@gmail.com>
2018-02-06 13:27:17 +0700

    fix stock

--- app/models/jde_item_availability.rb
+++ app/models/jde_item_availability.rb
@@ -34,7 +34,7 @@ class JdeItemAvailability < ActiveRecord::Base
     FROM PRODDTA.F41021 IA WHERE  
     NOT REGEXP_LIKE(liglpt, 'WIP|MAT')
      AND lipbin = 'S' AND liupmj = '#{date_to_julian(Date.today)}' AND litday >=
-    '#{2.minutes.ago.change(sec: 0).strftime('%k%M%S')}'
+    '#{15.minutes.ago.change(sec: 0).strftime('%k%M%S')}'
     GROUP BY IA.liitm, IA.limcu")
     us.each do |fus|
       stock = self.find_by_sql("SELECT IA.liitm AS liitm, 
