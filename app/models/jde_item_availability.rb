dbc2a151bf8f1719f8a48c2ab8858c525b27199d
adjidedjo <adji.el.djo@gmail.com>
2018-10-17 16:19:56 +0700

    fix import history

--- app/models/jde_item_availability.rb
+++ app/models/jde_item_availability.rb
@@ -239,6 +239,7 @@ class JdeItemAvailability < ActiveRecord::Base
       NVL(SUM(IL.ADJIN),0) AS QTY_ADJIN,
       NVL(SUM(IL.ADJOUT),0) AS QTY_ADJOUT,
       NVL(SUM(ST.LIPQOH),0) AS STOCK,
+      MAX(IM.IMPRGR) AS BRAND,
       MAX(IM.IMITM) AS SHORT FROM PRODDTA.F0006 MC
       LEFT JOIN
       (
@@ -246,7 +247,7 @@ class JdeItemAvailability < ActiveRecord::Base
       ) IB ON IB.IBMCU = MC.MCMCU
       LEFT JOIN
       (
-        SELECT IMITM, IMTMPL, IMDSC1, IMDSC2, IMLITM, IMSTKT FROM PRODDTA.F4101 WHERE IMTMPL LIKE '%BJ MATRASS%' AND IMSTKT != 'O'
+        SELECT IMITM, IMTMPL, IMPRGR, IMDSC1, IMDSC2, IMLITM, IMSTKT FROM PRODDTA.F4101 WHERE IMTMPL LIKE '%BJ MATRASS%' AND IMSTKT != 'O'
       ) IM ON IM.IMITM = IB.IBITM
       LEFT JOIN 
       (
@@ -262,13 +263,13 @@ class JdeItemAvailability < ActiveRecord::Base
           FROM PRODDTA.F4111 WHERE ILTRDJ = '#{date_to_julian(Date.yesterday)}' 
           AND ILDCT IN ('SO', 'ST', 'IA', 'IR', 'SK', 'RO', 'CO', 'OV') GROUP BY ILMCU, ILITM, ILDCT
       ) IL ON IM.IMITM = IL.ILITM AND MC.MCMCU = IL.ILMCU
-      WHERE MC.MCRP05 LIKE '510' AND ST.LIPQOH >= 0 GROUP BY IM.IMITM, ST.LIMCU, IL.ILMCU")
+      WHERE MC.MCRP05 LIKE '510' AND IL.QTYIN >= 0 OR IL.QTYOUT < 1 OR IL.ADJIN >= 1 OR IL.ADJOUT < 1 OR ST.LIPQOH > 0 GROUP BY IM.IMITM, ST.LIMCU, IL.ILMCU")
     us.each do |fus|
       ActiveRecord::Base.establish_connection("production").connection.execute("REPLACE INTO dbmarketing.historical_stocks (branch_plan, description,
-      item_number, stock, out_stock, in_stock, adj_in, adj_out, branch_id, branch_desc, transaction_date,
+      item_number, brand, stock, out_stock, in_stock, adj_in, adj_out, branch_id, branch_desc, transaction_date,
       short_item, created_at, updated_at, fday, fmonth, fyear) VALUES 
         ('#{fus.stock_branch.strip}', '#{fus.description}',
-        '#{fus.kode}', '#{fus.stock}', 
+        '#{fus.kode}', '#{fus.brand}', '#{fus.stock}', 
         '#{fus.qty_out}', '#{fus.qty_in}', '#{fus.qty_adjin}', '#{fus.qty_adjout}',
         '#{fus.stock_branch == '0' ? 0 : jde_cabang(fus.stock_branch.strip)}',
         '#{Cabang.branch_description(jde_cabang(fus.stock_branch.strip)).nil? ? '-' : Cabang.branch_description(jde_cabang(fus.stock_branch.strip)).Cabang}',
@@ -291,23 +292,23 @@ class JdeItemAvailability < ActiveRecord::Base
   end
   
   def self.jde_cabang(bu)
-    if bu == "11001" || bu == "11001D" || bu == "11001C" || bu == "18001" #pusat
+    if bu.include?('11002') || bu == "11001" || bu == "11001D" || bu == "11001C" || bu == "11001S" || bu == "18001" #pusat
       "01"
     elsif bu == "11101" || bu == "11102" || bu == "11101C" || bu == "11101D" || bu == "11101S" || bu == "18101" || bu == "18101C" || bu == "18101D" || bu == "18102" || bu == "18101S" || bu == "18101K" #lampung
       "13" 
-    elsif bu == "18011" || bu == "18011C" || bu == "18011D" || bu == "18012" || bu == "18011S" || bu == "18011K" #jabar
+    elsif bu.include?('13011') || bu.include?('11011') || bu == "18011" || bu == "18011C" || bu == "18011D" || bu == "18012" || bu == "18011S" || bu == "18011K" #jabar
       "02"
     elsif bu == "18021" || bu == "18021C" || bu == "18021D" || bu == "18022" || bu == "18021S" || bu == "18021K" #cirebon
       "09"
     elsif bu == "12001" || bu == "12002" || bu == "12001C" || bu == "12001D" #bestari mulia
       "50"
-    elsif bu == "12061" || bu == "12062" || bu == "12001" || bu == "12061C" || bu == "12061D" || bu == "12061S" || bu == "18061" || bu == "18061C" || bu == "18061D" || bu == "18061S" #surabaya
+    elsif bu.include?('13061') || bu == "12061" || bu == "12062" || bu == "12001" || bu == "12061C" || bu == "12061D" || bu == "12061S" || bu == "18061" || bu == "18061C" || bu == "18061D" || bu == "18061S" #surabaya
       "07"
-    elsif bu == "18151" || bu == "18151C" || bu == "18151D" || bu == "18152" || bu == "18151S" || bu == "18151K" || bu == "11151" #cikupa
+    elsif bu.include?('11052') ||  bu == "18151" || bu == "18151C" || bu == "18151D" || bu == "18152" || bu == "18151S" || bu == "18151K" || bu == "11151" #cikupa
       "23"
-    elsif bu == "11030" || bu == "18031" || bu == "18031C" || bu == "18031D" || bu == "18032" || bu == "18031S" || bu == "18031K" #narogong
+    elsif bu.include?('11031') || bu == "11030" || bu == "18031" || bu == "18031C" || bu == "18031D" || bu == "18032" || bu == "18031S" || bu == "18031K" #narogong
       "03"
-    elsif bu == "12111" || bu == "12112" || bu == "12111C" || bu == "12111D" || bu == "12111S" || bu == "18111" || bu == "18111C" || bu == "18111D" || bu == "18112" || bu == "18111S" || bu == "18111K" || bu == "18112C" || bu == "18112D" || bu == "18112K" #makasar
+    elsif bu == "13111D" || bu == "13111S" || bu == "12111" || bu == "12112" || bu == "12111C" || bu == "12111D" || bu == "12111S" || bu == "18111" || bu == "18111C" || bu == "18111D" || bu == "18112" || bu == "18111S" || bu == "18111K" || bu == "18112C" || bu == "18112D" || bu == "18112K" #makasar
       "19"
     elsif bu == "12071" || bu == "12072" || bu == "12071C" || bu == "12071D" || bu == "12071S" || bu == "18071" || bu == "18071C" || bu == "18071D" || bu == "18072" || bu == "18071S" || bu == "18071K" || bu == "18072C" || bu == "18071D" || bu == "18071K" #bali
       "04"
@@ -315,13 +316,13 @@ class JdeItemAvailability < ActiveRecord::Base
       "22" 
     elsif bu == "11091" || bu == "11092" || bu == "11091C" || bu == "11091D" || bu == "11091S" || bu == "18091" || bu == "18091C" || bu == "18091D" || bu == "18092" || bu == "18091S" || bu == "18091K" || bu == "18092C" || bu == "18092D" || bu == "18092K" #palembang
       "11"
-    elsif bu == "11041" || bu == "11042" || bu == "11041C" || bu == "11041D" || bu == "11041S" || bu == "18041" || bu == "18041C" || bu == "18041D" || bu == "18042" || bu == "18042S" || bu == "18042K" || bu == "18042C" || bu == "18042D" || bu == "18042K" #yogyakarta
+    elsif bu.include?('18041') || bu.include?('13041') || bu == "11041" || bu == "11042" || bu == "11041C" || bu == "11041D" || bu == "11041S" || bu == "18041" || bu == "18041C" || bu == "18041D" || bu == "18042" || bu == "18042S" || bu == "18042K" || bu == "18042C" || bu == "18042D" || bu == "18042K" #yogyakarta
       "10"
     elsif bu == "11051" || bu == "11052" || bu == "11051C" || bu == "11051D" || bu == "11051S" || bu == "18051" || bu == "18051C" || bu == "18051D" || bu == "18052" || bu == "18051S" || bu == "18051K" || bu == "18052C" || bu == "18052D" || bu == "18052K" #semarang
       "08"
     elsif bu == "11081" || bu == "11082" || bu == "11081C" || bu == "11081D" || bu == "11081S" || bu == "18081" || bu == "18081C" || bu == "18081D" || bu == "18082" || bu == "18081S" || bu == "18081K" || bu == "18082C" || bu == "18082D" || bu == "18082K" #medan
       "05"
-    elsif bu == "11121" || bu == "11122" || bu == "11121C" || bu == "11121D" || bu == "11121S" || bu == "18121" || bu == "18121C" || bu == "18121D" || bu == "18122" || bu == "18121S" || bu == "18121K" || bu == "18122C" || bu == "18122D" || bu == "18122K" #pekanbaru
+    elsif bu.include?('12112') || bu == "11121" || bu == "11122" || bu == "11121C" || bu == "11121D" || bu == "11121S" || bu == "18121" || bu == "18121C" || bu == "18121D" || bu == "18122" || bu == "18121S" || bu == "18121K" || bu == "18122C" || bu == "18122D" || bu == "18122K" #pekanbaru
       "20"
     elsif bu.include?('180120') || bu.include?("180110") #tasikmalaya
       "02"
