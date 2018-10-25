8f3cbf5fdc2a96d0e7c1cce3e4fa1100e5c04587
adjidedjo <adji.el.djo@gmail.com>
2018-10-25 14:23:32 +0700

    fix import orders

--- app/models/warehouse/f4211_order.rb
+++ app/models/warehouse/f4211_order.rb
@@ -30,10 +30,11 @@ class Warehouse::F4211Order < ActiveRecord::Base
         serial: r["SDLOTN"].strip, customer_po: r["SDVR01"].strip)
     end
     Production.production_import_outstanding_orders #import data outstanding for production
+    sales_mart_import_outstanding_orders_only_production
     sales_mart_import_outstanding_orders
   end
   
-  def self.sales_mart_import_outstanding_orders
+  def self.sales_mart_import_outstanding_orders_only_production
     ActiveRecord::Base.establish_connection("warehouse").connection.execute("
       DELETE FROM sales_mart.PRODUCTION_ORDERS WHERE DATE(created_at) = '#{2.days.ago.to_date}'
     ")
@@ -44,6 +45,26 @@ class Warehouse::F4211Order < ActiveRecord::Base
     AND DATE(created_at) = '#{Date.today}'")
     outstanding.each do |ou|
       ActiveRecord::Base.establish_connection("warehouse").connection.execute("INSERT INTO sales_mart.PRODUCTION_ORDERS (order_no, promised_delivery, branch, brand, item_number,
+      description, order_date, quantity, short_item, segment1, customer, ship_to, typ, last_status,
+      branch_desc, originator, exceeds, next_status, day_category, created_at) VALUES ('#{ou.order_no}', 
+      '#{ou.promised_delivery}', '#{ou.branch}', '#{ou.brand}', '#{ou.item_number}', '#{ou.description}',
+      '#{ou.order_date}', '#{ou.quantity}', '#{ou.short_item}', 
+      '#{ou.segment1}', '#{ou.customer}', '#{ou.ship_to}', '#{ou.typ}',
+      '#{ou.last_status}', '#{ou.branch_desc}', '#{ou.originator}',
+      '#{(Date.today - ou.promised_delivery)}', '#{ou.next_status}', 
+      '#{category_days((Date.today - ou.promised_delivery))}', '#{Date.today}')")
+    end
+  end
+  
+  def self.sales_mart_import_outstanding_orders
+    ActiveRecord::Base.establish_connection("warehouse").connection.execute("
+      DELETE FROM sales_mart.BRANCH_ORDERS WHERE DATE(created_at) = '#{2.days.ago.to_date}'
+    ")
+    outstanding = Warehouse::F4211Order.find_by_sql("
+    SELECT * FROM warehouse.F4211_ORDERS
+    WHERE typ = 'C' AND DATE(created_at) = '#{Date.today}'")
+    outstanding.each do |ou|
+      ActiveRecord::Base.establish_connection("warehouse").connection.execute("INSERT INTO sales_mart.BRANCH_ORDERS (order_no, promised_delivery, branch, brand, item_number,
       description, order_date, quantity, short_item, segment1, customer, ship_to, typ, last_status,
       branch_desc, originator, exceeds, next_status, day_category, created_at) VALUES ('#{ou.order_no}', 
       '#{ou.promised_delivery}', '#{ou.branch}', '#{ou.brand}', '#{ou.item_number}', '#{ou.description}',
