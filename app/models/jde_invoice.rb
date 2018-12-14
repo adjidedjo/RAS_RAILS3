df9ca720140d9debe668b51a519973a6601a7cc7
adjidedjo <adji.el.djo@gmail.com>
2018-12-14 12:26:54 +0700

    fix invoice

--- app/models/jde_invoice.rb
+++ app/models/jde_invoice.rb
@@ -2,6 +2,96 @@ class JdeInvoice < ActiveRecord::Base
   establish_connection "jdeoracle"
   self.table_name = "PRODDTA.F03B11" #rp
   
+  def self.trial_import
+    invoices = find_by_sql("SELECT SA.RPLNID AS LINEFAKTUR, SA.RPDOC AS NOFAKTUR, SA.RPDCT AS ORTY, SA.RPSDOC AS NOSO, SA.RPSDCT AS DOC, SA.RPSFX AS LINESO, 
+      SA.RPDIVJ AS TANGGALINVOICE, SA.RPPN AS MONTH, 
+      (SA.RPCTRY||SA.RPFY) AS YEAR, SA.RPU/100 AS JUMLAH, SA.RPAG AS TOTAL, 
+      SA.RPMCU AS BP, SA.RPAN8 AS KODECUSTOMER, SA.RPALPH AS CUSTOMER, CM.ABSIC AS TIPECUST, NVL(TRIM(CIT.ALCTY1), '-') AS KOTA, SM.SASLSM AS KODESALES, 
+      NVL(CM1.ABALPH, '-') AS NAMASALES,
+      IM.IMITM AS SHORTITEM, SA.RPRMK AS KODEBARANG, IM.IMDSC1 AS DSC1, IM.IMDSC2 AS DSC2, IM.IMPRGR AS BRAND, IM.IMSEG1 AS TIPE, 
+      JN.DRDL01 AS NAMATIPE, NVL(GI.DRDL01,'-') AS GROUPITEM, IM.IMSEG2 AS KODEARTIKEL, 
+      ART.DRDL01 AS ARTICLE, IM.IMSEG3 AS KODEKAIN, KA.DRDL01 AS KAIN, 
+      IM.IMSEG4 AS ST, IM.IMSEG5 AS PANJANG, IM.IMSEG6 AS LEBAR, SA.RPRMR1 AS REFEREN1, SA.RPVR01 AS REFEREN FROM
+      (
+      SELECT * FROM PRODDTA.F03B11 WHERE RPDIVJ BETWEEN '118335' AND '118347' AND REGEXP_LIKE(rpdct,'RI|RO|RM')
+      ) SA
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODDTA.F4101 WHERE IMTMPL LIKE '%BJ MATRASS%'
+      ) IM ON TRIM(IM.IMLITM) = TRIM(SA.RPRMK)
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODCTL.F0005 WHERE DRSY = '55' AND DRRT = 'JN'
+      ) JN ON JN.DRKY LIKE '%'||TRIM(IM.IMSEG1)
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODCTL.F0005 WHERE DRSY = '55' AND DRRT = 'AT'
+      ) ART ON ART.DRKY LIKE '%'||TRIM(IM.IMSEG2)
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODCTL.F0005 WHERE DRSY = '55' AND DRRT = 'KA'
+      ) KA ON KA.DRKY LIKE '%'||TRIM(IM.IMSEG3)
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODCTL.F0005 WHERE DRSY = '41' AND DRRT = 'S3'
+      ) GI ON GI.DRKY LIKE '%'||IM.IMSRP3||'%'
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODDTA.F0101
+      ) CM ON TRIM(SA.RPAN8) = TRIM(CM.ABAN8)
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODDTA.F0116
+      ) CIT ON TRIM(CIT.ALAN8) = TRIM(CM.ABAN8)
+      LEFT OUTER JOIN
+      (
+        SELECT SASLSM, SAIT44, SAAN8 FROM PRODDTA.F40344 WHERE SAEXDJ > (select 1000*(to_char(sysdate, 'yyyy')-1900)+to_char(sysdate, 'ddd') as julian from dual)
+      ) SM ON SM.SAAN8 = SA.RPAN8 AND SM.SAIT44 = IM.IMSRP1
+      LEFT OUTER JOIN
+      (
+        SELECT * FROM PRODDTA.F0101
+      ) CM1 ON TRIM(SM.SASLSM) = TRIM(CM1.ABAN8)
+      
+      WHERE IM.IMPRGR IS NOT NULL")
+    invoices.each do |iv|
+        check = SalesReport.find_by_sql("SELECT nofaktur, orty, lnid, harganetto2 FROM dbmarketing.sales_reports 
+        WHERE nofaktur = '#{iv.nofaktur.to_i}' 
+        AND orty = '#{iv.orty.strip}' AND kode_customer = '#{iv.kodecustomer.to_i}'  
+        AND lnid = '#{iv.lineso.to_i}' AND fiscal_month = '#{iv.month.to_i}'")
+        if check.present?
+          raise iv.total.inspect if iv.total.to_i != check.first.harganetto2
+        elsif check.empty?
+          cabang = jde_cabang(iv.bp.to_i.to_s.strip)
+          area = find_area(cabang)
+          fullnamabarang = "#{iv.dsc1.strip} " "#{iv.dsc2.strip}"
+          alamat_so = iv.orty == 'RI' ? (get_address_from_order(iv.noso, iv.doc).nil? ? '-' : get_address_from_order(iv.noso, iv.doc).address) : '-'
+          adj = import_adjustment(iv.linefaktur.to_i, iv.noso.to_i, iv.doc) #find price_adjustment
+          SalesReport.create!(cabang_id: cabang, noso: iv.nofaktur.to_i, tanggalsj: julian_to_date(iv.tanggalinvoice),
+            kodebrg: iv.kodebarang.strip, namabrg: fullnamabarang, kode_customer: iv.kodecustomer.to_i, customer: iv.customer, 
+            jumlah: iv.jumlah.to_s.gsub(/0/,"").to_i, satuan: "PC",
+            jenisbrgdisc: iv.brand.strip, kodejenis: iv.tipe.strip, jenisbrg: iv.namatipe.strip, kodeartikel: iv.kodeartikel, namaartikel: iv.article,
+            kodekain: iv.kodekain.strip, namakain: iv.kain.strip, panjang: iv.panjang.to_i, lebar: iv.lebar.to_i, namabrand: iv.groupitem.strip,
+            harganetto1: iv.total, harganetto2: iv.total, kota: iv.kota, tipecust: get_group_customer(iv.tipecust), 
+            ketppb: "",
+            salesman: iv.namasales, orty: iv.orty.strip, nopo: iv.kodesales, 
+            fiscal_year: iv.year,
+              fiscal_month: iv.month, week: julian_to_date(iv.tanggalinvoice).to_date.cweek,
+              area_id: area, ketppb: iv.bp.strip, tanggal: julian_to_date(iv.tanggalinvoice),
+              nofaktur: iv.nofaktur.to_i, lnid: iv.lineso, nosj: iv.linefaktur.to_i, alamatkirim: iv.doc,
+              alamat_so: alamat_so, reference: iv.referen1, customerpo_so: iv.referen,
+              diskon1: adj.nil? ? 0 : adj.diskon1,
+              diskon2: adj.nil? ? 0 : adj.diskon2,
+              diskon3: adj.nil? ? 0 : adj.diskon3,
+              diskon4: adj.nil? ? 0 : adj.diskon4,
+              diskon5: adj.nil? ? 0 : adj.diskon5,
+              diskonsum: adj.nil? ? 0 : adj.diskon6,
+              diskonrp: adj.nil? ? 0 : adj.diskon7,
+              cashback: adj.nil? ? 0 : adj.diskon8,
+              nupgrade: adj.nil? ? 0 : adj.diskon9)
+      end
+    end
+  end
+  
   def self.test_import_sales
     invoices = find_by_sql("SELECT * FROM PRODDTA.F03B11 WHERE
     rpdivj BETWEEN '#{date_to_julian('01/11/2018'.to_date)}' AND '#{date_to_julian('30/11/2018'.to_date)}'
@@ -194,6 +284,22 @@ class JdeInvoice < ActiveRecord::Base
     Customer.batch_customer_active
     Customer.batch_calculate_customer_active
     batch_transform
+  end
+  
+  def self.get_group_customer(grup)
+    if grup.strip == 'DEA' || grup.strip == 'RET'
+      'RETAIL'
+    elsif grup.strip == 'SHO'
+      'SHOWROOM'
+    elsif grup.strip == 'MOD'
+      'MODERN'
+    elsif grup.strip == 'DIR'
+      'DIRECT'
+    elsif grup.strip == 'PRO'
+      'PROJECT'
+    else
+      '-'
+    end
   end
 
   def self.get_address_from_order(order, orty)
