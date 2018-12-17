cc684e021b78f41c0ae7658021b1e919adaad397
adjidedjo <adji.el.djo@gmail.com>
2018-12-17 16:55:49 +0700

    fix invoice

--- app/models/jde_invoice.rb
+++ app/models/jde_invoice.rb
@@ -12,7 +12,7 @@ class JdeInvoice < ActiveRecord::Base
        ART.DRDL01 AS ARTICLE, IM.IMSEG3 AS KODEKAIN, KA.DRDL01 AS KAIN, 
        IM.IMSEG4 AS ST, IM.IMSEG5 AS PANJANG, IM.IMSEG6 AS LEBAR, (CASE WHEN SA.RPDCT = 'RM' THEN SUBSTR(SA.RPRMR1, 1, 8) ELSE SA.RPRMR1 END) AS REFEREN1, SA.RPVR01 AS REFEREN FROM
        (
-         SELECT * FROM PRODDTA.F03B11 WHERE RPDIVJ BETWEEN '118305' AND '118334' AND REGEXP_LIKE(rpdct,'RI|RM|RO')
+         SELECT * FROM PRODDTA.F03B11 WHERE RPDIVJ BETWEEN '118274' AND '118334' AND REGEXP_LIKE(rpdct,'RI|RM|RO')
        ) SA
        LEFT JOIN
        (
@@ -58,7 +58,7 @@ class JdeInvoice < ActiveRecord::Base
           fullnamabarang = "#{iv.dsc1.strip} " "#{iv.dsc2.strip}"
           alamat_so = iv.orty == 'RI' ? (get_address_from_order(iv.noso, iv.doc).nil? ? '-' : get_address_from_order(iv.noso, iv.doc).address) : '-'
           adj = import_adjustment(iv.linefaktur.to_i, iv.noso.to_i, iv.doc) #find price_adjustment
-          SalesReport.create!(cabang_id: cabang, noso: iv.nofaktur.to_i, tanggalsj: julian_to_date(iv.tanggalinvoice),
+          LaporanCabang.create!(cabang_id: cabang, noso: iv.nofaktur.to_i, tanggalsj: julian_to_date(iv.tanggalinvoice),
             kodebrg: iv.kodebarang.strip, namabrg: fullnamabarang, kode_customer: iv.kodecustomer.to_i, customer: iv.customer, 
             jumlah: iv.jumlah.to_s.gsub(/0/,"").to_i, satuan: "PC",
             jenisbrgdisc: iv.brand.strip, kodejenis: iv.tipe.strip, jenisbrg: iv.namatipe.strip, kodeartikel: iv.kodeartikel, namaartikel: iv.article,
@@ -68,18 +68,40 @@ class JdeInvoice < ActiveRecord::Base
             salesman: iv.namasales, orty: iv.orty.strip, nopo: iv.kodesales, 
             fiscal_year: julian_to_date(iv.tanggalinvoice).to_date.year,
             fiscal_month: julian_to_date(iv.tanggalinvoice).to_date.month, week: julian_to_date(iv.tanggalinvoice).to_date.cweek,
-            area_id: area, ketppb: iv.bp.strip, tanggal: julian_to_date(iv.tanggalinvoice),
-            nofaktur: iv.nofaktur.to_i, lnid: iv.lineso, nosj: iv.linefaktur.to_i, alamatkirim: iv.doc,
-            alamat_so: alamat_so, reference: iv.referen1, customerpo_so: iv.referen,
-            diskon1: adj.nil? ? 0 : adj.diskon1,
-            diskon2: adj.nil? ? 0 : adj.diskon2,
-            diskon3: adj.nil? ? 0 : adj.diskon3,
-            diskon4: adj.nil? ? 0 : adj.diskon4,
-            diskon5: adj.nil? ? 0 : adj.diskon5,
-            diskonsum: adj.nil? ? 0 : adj.diskon6,
-            diskonrp: adj.nil? ? 0 : adj.diskon7,
-            cashback: adj.nil? ? 0 : adj.diskon8,
-            nupgrade: adj.nil? ? 0 : adj.diskon9)
+              area_id: area, ketppb: iv.bp.strip, tanggal: julian_to_date(iv.tanggalinvoice),
+              nofaktur: iv.nofaktur.to_i, lnid: iv.lineso, nosj: iv.linefaktur.to_i, alamatkirim: iv.doc,
+              alamat_so: alamat_so, reference: iv.referen1, customerpo_so: iv.referen,
+              diskon1: adj.nil? ? 0 : adj.diskon1,
+              diskon2: adj.nil? ? 0 : adj.diskon2,
+              diskon3: adj.nil? ? 0 : adj.diskon3,
+              diskon4: adj.nil? ? 0 : adj.diskon4,
+              diskon5: adj.nil? ? 0 : adj.diskon5,
+              diskonsum: adj.nil? ? 0 : adj.diskon6,
+              diskonrp: adj.nil? ? 0 : adj.diskon7,
+              cashback: adj.nil? ? 0 : adj.diskon8,
+              nupgrade: adj.nil? ? 0 : adj.diskon9)
+          Warehouse::Invoice.create!(cabang_id: cabang, noso: iv.nofaktur.to_i, tanggalsj: julian_to_date(iv.tanggalinvoice),
+            kodebrg: iv.kodebarang.strip, namabrg: fullnamabarang, kode_customer: iv.kodecustomer.to_i, customer: iv.customer, 
+            jumlah: iv.jumlah.to_s.gsub(/0/,"").to_i, satuan: "PC",
+            jenisbrgdisc: iv.brand.strip, kodejenis: iv.tipe.strip, jenisbrg: iv.namatipe.strip, kodeartikel: iv.kodeartikel, namaartikel: iv.article,
+            kodekain: iv.kodekain.strip, namakain: iv.kain.strip, panjang: iv.panjang.to_i, lebar: iv.lebar.to_i, namabrand: iv.groupitem.strip,
+            harganetto1: iv.total, harganetto2: iv.total, kota: iv.kota, tipecust: get_group_customer(iv.tipecust), 
+            ketppb: "",
+            salesman: iv.namasales, orty: iv.orty.strip, nopo: iv.kodesales, 
+            fiscal_year: julian_to_date(iv.tanggalinvoice).to_date.year,
+            fiscal_month: julian_to_date(iv.tanggalinvoice).to_date.month, week: julian_to_date(iv.tanggalinvoice).to_date.cweek,
+              area_id: area, ketppb: iv.bp.strip, tanggal: julian_to_date(iv.tanggalinvoice),
+              nofaktur: iv.nofaktur.to_i, lnid: iv.lineso, nosj: iv.linefaktur.to_i, alamatkirim: iv.doc,
+              alamat_so: alamat_so, reference: iv.referen1, customerpo_so: iv.referen,
+              diskon1: adj.nil? ? 0 : adj.diskon1,
+              diskon2: adj.nil? ? 0 : adj.diskon2,
+              diskon3: adj.nil? ? 0 : adj.diskon3,
+              diskon4: adj.nil? ? 0 : adj.diskon4,
+              diskon5: adj.nil? ? 0 : adj.diskon5,
+              diskonsum: adj.nil? ? 0 : adj.diskon6,
+              diskonrp: adj.nil? ? 0 : adj.diskon7,
+              cashback: adj.nil? ? 0 : adj.diskon8,
+              nupgrade: adj.nil? ? 0 : adj.diskon9)
     end
   end
 
