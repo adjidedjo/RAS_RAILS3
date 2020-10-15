8a4b33bbc66ef05ee2466dff91ceb698a3d2efcf
adjidedjo <adji.el.djo@gmail.com>
2020-10-15 13:52:46 +0700

    add feature insert pos data to jde

--- app/models/jde_invoice_processing.rb
+++ app/models/jde_invoice_processing.rb
@@ -1,15 +1,19 @@
 class JdeInvoiceProcessing < ActiveRecord::Base
   establish_connection "jdeoracle"
-#  self.abstract_class = true
+  #  self.abstract_class = true
   self.table_name = "PRODDTA.F03B11"
-  
   def self.insert_pos_to_jde(pos)
-    connection.execute("
-      INSERT INTO PRODDTA.F55ADD1(ECVR01, ECC75PNAME, ECCF01, ECPH1, ECTX2) 
-      VALUES (#{pos.map{|x| x.inspect}.join(', ')})
+    check = connection.execute("
+      SELECT * FROM PRODDTA.F55ADD1 WHERE ECVR01 = '#{pos.first}'
     ")
+    if check.nil?
+      connection.execute("
+        INSERT INTO PRODDTA.F55ADD1(ECVR01, ECC75PNAME, ECCF01, ECPH1, ECTX2)
+        VALUES (#{pos.map{|x| x.inspect}.join(', ')})
+      ")
+    end
   end
-  
+
   def self.credit_memo
     cm = self.find_by_sql("SELECT rpdivj, rpag, rpsdoc,rprmk FROM PRODDTA.F03B11 WHERE rpdivj = '#{date_to_julian('16/02/2017'.to_date)}'")
     cm.each do |memo|
