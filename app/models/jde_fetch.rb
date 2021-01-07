393cff46a50b9435ce9e1ffbe381f25c41dd09fd
adjidedjo <adji.el.djo@gmail.com>
2021-01-07 13:27:50 +0700

    fix

--- app/models/jde_fetch.rb
+++ app/models/jde_fetch.rb
@@ -7,8 +7,8 @@ class JdeFetch < ActiveRecord::Base
        SA.SDTRDJ AS TANGGALORDER, SUM(SA.SDPQOR) AS TOTALJUMLAH, SUM(SA.SDAEXP) AS TOTAL
        FROM
        (
-         SELECT * FROM PRODDTA.F4211 WHERE SDTRDJ BETWEEN  '#{date_to_julian(Date.yesterday.to_date)}' AND
-         '#{date_to_julian(Date.today.to_date)}'
+         SELECT * FROM PRODDTA.F4211 WHERE SDTRDJ BETWEEN  '#{date_to_julian(Date.today.beginning_of_month.to_date)}' AND
+         '#{date_to_julian(Date.today.end_of_month.to_date)}'
          AND REGEXP_LIKE(SDDCTO,'SO|ZO') AND SDLTTR != '980'
        ) SA
        LEFT JOIN
