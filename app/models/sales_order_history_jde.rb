c7ab2fc328bb8435c37e27c579bd2bee4735f036
adjidedjo <adji.el.djo@gmail.com>
2022-12-05 13:36:53 +0700

    fix

--- app/models/sales_order_history_jde.rb
+++ app/models/sales_order_history_jde.rb
@@ -5,7 +5,7 @@ class SalesOrderHistoryJde < ActiveRecord::Base
 
   scope :delivered, -> { where("sdnxtr >= ? and sdlttr >= ? and regexp_like(sddcto, ?)", "580", "565", 'SK|ST') }
 
-  def self.find_sales_transfer_to_showroom(date, date_to, showroom_id)
+  def self.find_sales_transfer_to_showroom(date, showroom_id)
     find_by_sql("SELECT SDMCU, SDDOCO, SDADDJ, SDLITM, SDDSC1, SDDSC2, SDLOTN, SDDELN, SDSOQS FROM PRODDTA.F4211
  		WHERE REGEXP_LIKE(SDDCTO, 'SK|ST') 
     		AND SDADDJ = '#{date_to_julian(date)}' AND SDSHAN LIKE '#{showroom_id}' 
