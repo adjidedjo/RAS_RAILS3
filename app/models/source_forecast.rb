f8b5108d19ebd0f7e04b4270e971345be19065bb
adjidedjo <adji.el.djo@gmail.com>
2023-04-04 09:38:12 +0700

    fix

--- app/models/source_forecast.rb
+++ app/models/source_forecast.rb
@@ -8,12 +8,15 @@ class SourceForecast < ActiveRecord::Base
         description = ((dsc1.nil? ? '' : dsc1.strip) + ' ' + (dsc2.nil? ? '' : dsc2.strip))
         find_area = ActiveRecord::Base.connection.execute("SELECT area_id FROM dbmarketing.gudangs WHERE code = '#{cabang_id}'")
         area_id = find_area.first.nil? ? branch : find_area.first[0]
+	bom = ActiveRecord::Base.connection.execute("SELECT bom_id, bom_name FROM sales_mart.branch_of_manager WHERE sales_id = '#{kodesales}'")
+        bom_id = bom.first.nil? ? '' : bom.first[0]
+        bom_name = bom.first.nil? ? '' : bom.first[1]
         ActiveRecord::Base.connection.execute("REPLACE INTO sales_mart.DETAIL_SALES_FOR_FORECASTS (nopo, salesman, 
             item_number, brand, segment1_code, segment2_code, segment3_code, panjang, lebar, 
-            product_name, area_id, customer_type, invoice_date, month, year, week, total, bp)
+            product_name, area_id, customer_type, invoice_date, month, year, week, total, bp, bom, bom_name)
             VALUES ('#{kodesales}', '#{namasales}', 
                 '#{item_number}', '#{group_forecast}', '#{tipe.strip}', '#{article.strip}', '#{kain}', '#{panjang.strip}', '#{lebar.strip}', 
                 '#{description}', '#{area_id}', '#{tipecust}', '#{invoicedate}', '#{fmonth}', '#{year}', '#{fweek}', '#{jumlah}', 
-                #{cabang_id}) ")                
+                #{cabang_id}, '#{bom_id}', '#{bom_name}') ")                
     end
 end
